c25556102b1f54b9682b87aa82d18a8e338752ea
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-09-11 13:49:44 +0700

    Song Huong push nham

---
+++ EVCare_BackEnd/DataAccess/EVCareDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class EVCareDbContext : DbContext, IEVCareDbContext
+    {
+        public EVCareDbContext(DbContextOptions<EVCareDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Alert> Alerts { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<AppointmentImages> AppointmentImages { get; set; }
+        public DbSet<AppointmentService> AppointmentServices { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Invoices> Invoices { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderParts> OrderParts { get; set; }
+
+    }
+}
