306c2424b497ae215a20dc712a8b863152501175
slazenger1906 <npsdn1906@gmail.com>
2025-09-11 15:16:56 +0700

    add migration

--- EVCare_BackEnd/DataAccess/EVCareDbContext.cs
+++ EVCare_BackEnd/DataAccess/EVCareDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Configuration;
 using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,20 +11,37 @@ namespace DataAccess
 {
     public class EVCareDbContext : DbContext, IEVCareDbContext
     {
+  
         public EVCareDbContext(DbContextOptions<EVCareDbContext> options) : base(options)
         {
         }
-        public DbSet<Account> Accounts { get; set; }
-        public DbSet<Alert> Alerts { get; set; }
-        public DbSet<Application> Applications { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
-        public DbSet<AppointmentImages> AppointmentImages { get; set; }
-        public DbSet<AppointmentService> AppointmentServices { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Invoices> Invoices { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderParts> OrderParts { get; set; }
+
+        //public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehiclesCategory> VehiclesCategories { get; set; }
+
+        //public DbSet<Account> Accounts { get; set; }
+        //public DbSet<Alert> Alerts { get; set; }
+        //public DbSet<Application> Applications { get; set; }
+        //public DbSet<Appointment> Appointments { get; set; }
+        //public DbSet<AppointmentImages> AppointmentImages { get; set; }
+        //public DbSet<AppointmentService> AppointmentServices { get; set; }
+        //public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Employee> Employees { get; set; }
+        //public DbSet<Invoices> Invoices { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderParts> OrderParts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=evcare-db.xxxxx.ap-southeast-1.rds.amazonaws.com,1433;Database=EVCare;User Id=admin;Password=Top1swp391herewego;Encrypt=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new VehicleCategoryConfiguration());
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
 
     }
 }
--- EVCare_BackEnd/DataAccess/IEVCareDbContext.cs
+++ EVCare_BackEnd/DataAccess/IEVCareDbContext.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
 {
     public interface IEVCareDbContext
     {
+        //DbSet<Vehicle> Vehicles { get; set; }
+        DbSet<VehiclesCategory> VehiclesCategories { get; set; }
+        //DbSet<Account> Accounts { get; set; }
+        //DbSet<Alert> Alerts { get; set; }
+        //DbSet<Application> Applications { get; set; }
+        //DbSet<Appointment> Appointments { get; set; }
+        //DbSet<AppointmentImages> AppointmentImages { get; set; }
+        //DbSet<AppointmentService> AppointmentServices { get; set; }
+        //DbSet<Customer> Customers { get; set; }
+        //DbSet<Employee> Employees { get; set; }
+        //DbSet<Invoices> Invoices { get; set; }
+        //DbSet<Order> Orders { get; set; }
+        //DbSet<OrderParts> OrderParts { get; set; }
+        public Task<int> SaveChangesAsync();
     }
 }
