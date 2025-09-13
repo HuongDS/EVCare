f8912b51813a66d8ab47b085c687f1eab307c911
slazenger1906 <npsdn1906@gmail.com>
2025-09-13 21:34:52 +0700

    add Price Columns for orderpart

--- EVCare_BackEnd/DataAccess/Entities/OrderPart.cs
+++ EVCare_BackEnd/DataAccess/Entities/OrderPart.cs
@@ -14,5 +14,6 @@ namespace DataAccess.Entities
         public int OrderId { get; set; }
         public Order Order { get; set; }    
         public int Quantity { get; set; }
+        public decimal Price { get; set; }
     }
 }
