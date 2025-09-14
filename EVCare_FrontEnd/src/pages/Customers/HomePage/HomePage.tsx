3f4398e81c448c235b8bea88add331d53e1f3cb6
vyngochi <ncv.ngovy@gmail.com>
2025-09-14 23:30:18 +0700

    Code Header, Footer, Hero Section for Landing Page

--- EVCare_FrontEnd/src/pages/Customers/HomePage/HomePage.tsx
+++ EVCare_FrontEnd/src/pages/Customers/HomePage/HomePage.tsx
@@ -1,21 +1,17 @@
 import Hero from "./sections/Hero";
-import Header from "./../../components/Header/Header";
-import Footer from "../../components/Footer/Footer";
 import WhyEVCare from "./sections/WhyEVCare";
 import MaintenanceBanner from "./sections/MaintenanceBanner";
 import AboutUs from "./sections/AboutUs";
 import Brands from "./sections/Brands";
 
-export default function index() {
+export default function HomePage() {
   return (
     <>
-      <Header></Header>
       <Hero></Hero>
       <WhyEVCare></WhyEVCare>
       <MaintenanceBanner></MaintenanceBanner>
       <AboutUs></AboutUs>
       <Brands></Brands>
-      <Footer></Footer>
     </>
   );
 }
