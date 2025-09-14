62f3bd10df40ba0222e3f2bff4c939c9ccfbbfd4
vyngochi <ncv.ngovy@gmail.com>
2025-09-14 11:32:25 +0700

    Add sections to index.tsx

--- EVCare_FrontEnd/src/pages/HomePage/index.tsx
+++ EVCare_FrontEnd/src/pages/HomePage/index.tsx
@@ -1,12 +1,20 @@
 import Hero from "./sections/Hero";
 import Header from "./../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import WhyEVCare from "./sections/WhyEVCare";
+import MaintenanceBanner from "./sections/MaintenanceBanner";
+import AboutUs from "./sections/AboutUs";
+import Brands from "./sections/Brands";
 
 export default function index() {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
+      <WhyEVCare></WhyEVCare>
+      <MaintenanceBanner></MaintenanceBanner>
+      <AboutUs></AboutUs>
+      <Brands></Brands>
       <Footer></Footer>
     </>
   );
