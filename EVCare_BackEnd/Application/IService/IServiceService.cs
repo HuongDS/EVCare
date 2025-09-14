ee13b9df1f90b9b31ee6fc4ea77f7f3924c49696
slazenger1906 <npsdn1906@gmail.com>
2025-09-14 22:25:21 +0700

    add service and controller

---
+++ EVCare_BackEnd/Application/IService/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Dtos.Service;
+
+namespace Application.IService
+{
+    public interface IServiceService
+    {
+        Task<IEnumerable<ServiceViewModel>> GetAllServicesAsync();
+        //Task<ServiceDto> GetServiceByIdAsync(int id);
+        //Task<IEnumerable<ServiceDto>> GetServicesWithPaginationAsync(int payload, int payindex);
+        //Task<ServiceDto> AddServiceAsync(CreateServiceDto createServiceDto);
+        //Task<ServiceDto> UpdateServiceAsync(int id, UpdateServiceDto updateServiceDto);
+        //Task DeleteServiceAsync(int id);
+    }
+}
