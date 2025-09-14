f8749630e57caa2424870f14654b447642fae297
vyngochi <ncv.ngovy@gmail.com>
2025-09-15 00:39:53 +0700

    merge branch BinhPhuong to ChiVy

--- EVCare_FrontEnd/src/pages/Customers/HomePage/sections/AboutUs.tsx
+++ EVCare_FrontEnd/src/pages/Customers/HomePage/sections/AboutUs.tsx
@@ -1,31 +1,93 @@
-import React from "react";
-import styles from "../CSS/AboutUs.module.css";
+import styled from "styled-components";
+
+const WrapperAboutUs = styled.div`
+  font-family: "Outfit", sans-serif;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  background: #ffffff;
+  margin-left: 200px;
+  margin-right: 182px;
+`;
+
+const AboutUsTextWrapper = styled.div`
+  flex: 1;
+  max-width: 600px;
+`;
+
+const Title = styled.h2`
+  font-size: 50px;
+  font-weight: 800;
+  margin-top: 100px;
+  margin-bottom: 30px;
+  text-transform: uppercase;
+  text-align: left;
+  letter-spacing: 2px;
+`;
+
+const AboutUsText = styled.p`
+  font-size: 40px;
+  font-weight: 600;
+  text-align: left;
+  margin: 0;
+`;
+
+const HighlightBlue = styled.span`
+  color: #0056d2;
+  font-weight: 700;
+`;
+
+const AboutUsDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  max-width: 40%;
+  margin-left: 100px;
+`;
+
+const AboutUsDetailNumber = styled.p`
+  font-size: 100px;
+  font-weight: bold;
+  color: #000000;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+const AboutUsDetailDescription = styled.p`
+  font-size: 30px;
+  font-weight: 1000;
+  color: #00ad4e;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
 export default function AboutUs() {
   return (
-    <div className={styles.wrapperAboutUs}>
-      <div className={styles.aboutUs}>
-        <h2 className={styles.title}>ABOUT US</h2>
-        <p className={styles.aboutUsText}>
-          We are committed to delivering{" "}
-          <span className={styles.highlightBlue}>reliable</span>,{" "}
-          <span className={styles.highlightBlue}>transparent</span>, and{" "}
-          <span className={styles.highlightBlue}>tech-driven</span> EV
-          maintenance services, ensuring every journey is safe and worry-free.
-        </p>
-      </div>
+    <WrapperAboutUs>
+      <AboutUsTextWrapper>
+        <Title>ABOUT US</Title>
+        <AboutUsText>
+          We are committed to delivering <HighlightBlue>reliable</HighlightBlue>
+          , <HighlightBlue>transparent</HighlightBlue>, and{" "}
+          <HighlightBlue>tech-driven</HighlightBlue> EV maintenance services,
+          ensuring every journey is safe and worry-free.
+        </AboutUsText>
+      </AboutUsTextWrapper>
 
-      <div className={styles.aboutUsDetail}>
-        <p className={styles.aboutUsDetailNumber}>5,000+</p>
-        <p className={styles.aboutUsDetailDescription}>
+      <AboutUsDetail>
+        <AboutUsDetailNumber>5,000+</AboutUsDetailNumber>
+        <AboutUsDetailDescription>
           Electric Vehicles Serviced
-        </p>
-        <p className={styles.aboutUsDetailNumber}>50+</p>
-        <p className={styles.aboutUsDetailDescription}>Certified Technicians</p>
-        <p className={styles.aboutUsDetailNumber}>10</p>
-        <p className={styles.aboutUsDetailDescription}>
+        </AboutUsDetailDescription>
+        <AboutUsDetailNumber>50+</AboutUsDetailNumber>
+        <AboutUsDetailDescription>
+          Certified Technicians
+        </AboutUsDetailDescription>
+        <AboutUsDetailNumber>10</AboutUsDetailNumber>
+        <AboutUsDetailDescription>
           Years of Trusted Service
-        </p>
-      </div>
-    </div>
+        </AboutUsDetailDescription>
+      </AboutUsDetail>
+    </WrapperAboutUs>
   );
 }
--- EVCare_FrontEnd/src/pages/Customers/HomePage/sections/WhyEVCare.tsx
+++ EVCare_FrontEnd/src/pages/Customers/HomePage/sections/WhyEVCare.tsx
@@ -1,37 +1,89 @@
-import styles from "../CSS/WhyEVCare.module.css";
-import pic from "../../../assets/User-check.png";
+import styled from "styled-components";
+import pic from "../../../../assets/User-check.png";
+
+const Container = styled.div`
+  font-family: "Outfit", sans-serif;
+  padding: 40px;
+  background-color: #e7f0e6;
+  text-align: center;
+  margin-left: 182px;
+  margin-right: 182px;
+  border-radius: 30px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+  margin-bottom: 40px;
+  color: #00ad4e;
+`;
+
+const CardsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 20px;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  width: 280px;
+  height: 287px;
+  background: #0039a6;
+  padding: 20px;
+  margin: 20px auto;
+  font-size: 20px;
+  border-radius: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  color: white;
+  text-align: center;
+`;
+
+const Icon = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-bottom: 20px;
+  color: #ffffff;
+`;
+
+const TitleCard = styled.h2`
+  font-size: 15px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
 export default function WhyEVCare() {
   return (
-    <div className={styles.container}>
-      <h1>Why EVCare?</h1>
-      <div className={styles.cardsWrapper}>
-        <div className={styles.card}>
-          <img className={styles.icon} src={pic} />
-          <h2 className={styles.titleCard}>Expert EV Technicians</h2>
+    <Container>
+      <Title>Why EVCare?</Title>
+      <CardsWrapper>
+        <Card>
+          <Icon src={pic} />
+          <TitleCard>Expert EV Technicians</TitleCard>
           <p>
             Certified specialists with deep knowledge of electric vehicles to
             ensure reliable repairs and maintenance.
           </p>
-        </div>
-        <div className={styles.card}>
-          <img className={styles.icon} src={pic} />
-          <h2 className={styles.titleCard}>
-            Genuine Parts & Inventory Control
-          </h2>
+        </Card>
+
+        <Card>
+          <Icon src={pic} />
+          <TitleCard>Genuine Parts & Inventory Control</TitleCard>
           <p>
             We use high-quality EV spare parts with optimized stock management
             to keep your car running smoothly.
           </p>
-        </div>
-        <div className={styles.card}>
-          <img className={styles.icon} src={pic} />
-          <h2 className={styles.titleCard}>Clear Pricing & Digital Payments</h2>
+        </Card>
+
+        <Card>
+          <Icon src={pic} />
+          <TitleCard>Clear Pricing & Digital Payments</TitleCard>
           <p>
             Get upfront quotes, track your expenses, and pay easily through
             secure online methods.
           </p>
-        </div>
-      </div>
-    </div>
+        </Card>
+      </CardsWrapper>
+    </Container>
   );
 }
