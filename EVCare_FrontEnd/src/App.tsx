b71ecab4c70ac5f53d3e60db1a008081b4d0dbb9
vyngochi <ncv.ngovy@gmail.com>
2025-09-14 11:20:30 +0700

    Add HomePage.css

--- EVCare_FrontEnd/src/App.tsx
+++ EVCare_FrontEnd/src/App.tsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header></Header>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
--- EVCare_FrontEnd/src/pages/HomePage/index.tsx
+++ EVCare_FrontEnd/src/pages/HomePage/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import Hero from "./sections/Hero";
+import Header from "./../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default function index() {
   return (
     <>
+      <Header></Header>
       <Hero></Hero>
+      <Footer></Footer>
     </>
   );
 }
