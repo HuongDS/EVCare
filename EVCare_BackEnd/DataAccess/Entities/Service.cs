78fab0cdc4c02939299658d861f38cb45f5a0e07
slazenger1906 <npsdn1906@gmail.com>
2025-09-14 22:25:21 +0700

    add service and controller

--- EVCare_BackEnd/DataAccess/Entities/Service.cs
+++ EVCare_BackEnd/DataAccess/Entities/Service.cs
@@ -7,11 +7,9 @@ using DataAccess.Interfaces;
 
 namespace DataAccess.Entities
 {
-    public class Service : IEntity, ICreate, IUpdate, IDelete, ICategory
+    public class Service : IEntity, ICreate, IUpdate, IDelete
     {
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        //public ServiceCategory Category { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Duration { get; set; }
