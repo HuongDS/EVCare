9485d5a85392af8108b5081d878fe069cd357b02
slazenger1906 <npsdn1906@gmail.com>
2025-09-17 00:09:24 +0700

    add swagger gen

--- EVCare_BackEnd/API/Program.cs
+++ EVCare_BackEnd/API/Program.cs
@@ -16,6 +16,7 @@ using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -106,6 +107,34 @@ builder.Services.AddAuthentication(opt =>
     opt.ClientId = builder.Configuration["Authentication:Google:ClientId"];
     opt.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
     opt.CallbackPath = "/google-callback";
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "EVCare API", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Nhập 'Bearer {token}' vào đây"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 builder.Services.AddAuthorization();
