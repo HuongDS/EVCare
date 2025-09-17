761cb69dbdb739743f4c9978cf2ac988c3f6ae38
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-09-17 19:51:40 +0700

    authcontroller

--- EVCare_BackEnd/DataAccess/Interfaces/IEmployeeRepository.cs
+++ EVCare_BackEnd/DataAccess/Interfaces/IEmployeeRepository.cs
@@ -9,5 +9,6 @@ namespace DataAccess.Interfaces
 {
     public interface IEmployeeRepository : IGenericRepository<Employee>
     {
+        Task<Employee?> GetEmployeeByAccountId(int accountId);
     }
 }
