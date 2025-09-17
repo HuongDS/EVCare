8e574d21084d6c34b7cf02ee391f8ab0c2325c83
slazenger1906 <npsdn1906@gmail.com>
2025-09-17 20:27:56 +0700

    commit

--- EVCare_BackEnd/API/Controllers/AppointmentController.cs
+++ EVCare_BackEnd/API/Controllers/AppointmentController.cs
@@ -1,10 +1,12 @@
 ﻿using Application.Interfaces;
 using DataAccess.Dtos.Appointment;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AppointmentController : ControllerBase
@@ -14,6 +16,7 @@ namespace API.Controllers
         {
             _appointmentService = appointmentService;
         }
+        [Authorize(Roles = "Staff")]
         [HttpPost]
         public async Task<IActionResult> CreateAppointment(AppointmentCreateModel model)
         {
