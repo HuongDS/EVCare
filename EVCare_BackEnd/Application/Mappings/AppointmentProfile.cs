a10e440d430cf6e04cc1c3ed262a2074ba431d6a
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-09-19 21:50:06 +0700

    fix conflict

--- EVCare_BackEnd/Application/Mappings/AppointmentProfile.cs
+++ EVCare_BackEnd/Application/Mappings/AppointmentProfile.cs
@@ -34,29 +34,29 @@ namespace Application.Mappings
              }));
             CreateMap<AppointmentUpdateModel, Appointment>();
 
-CreateMap<Appointment, AppointmentViewModel>()
-                .ForMember(dest => dest.techinicianNames, opt =>
-                opt.MapFrom<TechnicianNameResolver>())
-                .ForMember(dest => dest.services, opt =>
-                opt.MapFrom<ServiceNameResolver>())
-                .ForMember(dest => dest.appointmentID, opt =>
-                opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.appointmentDate, opt =>
-                opt.MapFrom(src => src.Appointment_Date))
-                .ForMember(dest => dest.status, opt =>
-                opt.MapFrom(src => src.Status.ToString()))
-                .ForMember(dest => dest.customerName, opt =>
-                opt.MapFrom<CustomerNameResolver>())
-                .ForMember(dest => dest.customerPhone, opt =>
-                opt.MapFrom<CustomerPhoneResolver>())
-                .ForMember(dest => dest.vehiclePlate, opt =>
-                opt.MapFrom<VehiclePlateResolver>())
-                .ForMember(dest => dest.vehicleModel, opt =>
-                opt.MapFrom<VehicleModelResolver>())
-                .ForMember(dest => dest.note, opt =>
-                opt.MapFrom(src => src.Note))
-                .ForMember(dest => dest.employeeName, opt =>
-                opt.MapFrom<EmployeeNameResolver>());
+            CreateMap<Appointment, AppointmentViewDto>()
+                            .ForMember(dest => dest.techinicianNames, opt =>
+                            opt.MapFrom<TechnicianNameResolver>())
+                            .ForMember(dest => dest.services, opt =>
+                            opt.MapFrom<ServiceNameResolver>())
+                            .ForMember(dest => dest.appointmentID, opt =>
+                            opt.MapFrom(src => src.Id))
+                            .ForMember(dest => dest.appointmentDate, opt =>
+                            opt.MapFrom(src => src.Appointment_Date))
+                            .ForMember(dest => dest.status, opt =>
+                            opt.MapFrom(src => src.Status.ToString()))
+                            .ForMember(dest => dest.customerName, opt =>
+                            opt.MapFrom<CustomerNameResolver>())
+                            .ForMember(dest => dest.customerPhone, opt =>
+                            opt.MapFrom<CustomerPhoneResolver>())
+                            .ForMember(dest => dest.vehiclePlate, opt =>
+                            opt.MapFrom<VehiclePlateResolver>())
+                            .ForMember(dest => dest.vehicleModel, opt =>
+                            opt.MapFrom<VehicleModelResolver>())
+                            .ForMember(dest => dest.note, opt =>
+                            opt.MapFrom(src => src.Note))
+                            .ForMember(dest => dest.employeeName, opt =>
+                            opt.MapFrom<EmployeeNameResolver>());
         }
     }
 }
---
+++ EVCare_BackEnd/DataAccess/Dtos/Appointment/AppointmentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Enums;
+
+namespace DataAccess.Dtos.Appointment
+{
+    public class AppointmentViewModel
+    {
+        public int Id { get; set; }
+        public DateTime AppointmentDate { get; set; }
+
+        public string VehicleName { get; set; }
+        public List<string> Services { get; set; }
+        public string VehicleImageUrl { get; set; }
+        public AppointmentStatusEnum Status { get; set; }
+    }
+}
