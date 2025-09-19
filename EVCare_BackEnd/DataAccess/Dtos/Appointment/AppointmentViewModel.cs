60d9cd3f568a2d1752be2f079c5dadac18be14af
slazenger1906 <npsdn1906@gmail.com>
2025-09-19 21:33:52 +0700

    commit appointmentviewmodel

---
+++ EVCare_BackEnd/DataAccess/Dtos/Appointment/AppointmentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Enums;
+using Microsoft.Identity.Client;
+
+namespace DataAccess.Dtos.Appointment
+{
+    public class AppointmentViewModel
+    {
+        public int Id { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        
+        public string VehicleName { get; set; }
+        public List<string> Services { get; set; }
+        public string VehicleImageUrl { get; set; }
+        public AppointmentStatusEnum Status { get; set; }
+
+
+
+    }
+}
