3592bb1910a5e7872d540457c5400612783de494
vyngochi <ncv.ngovy@gmail.com>
2025-09-21 22:11:36 +0700

    feature: ServiceList.tsx - Show all service in Sevices Page (Mock Data, Continuing Update)

--- EVCare_FrontEnd/src/utils/router.tsx
+++ EVCare_FrontEnd/src/utils/router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../../src/pages/Users/HomePage/HomePage";
 import Layout from "../../src/components/Layouts/CustomerLayout";
-import Test from "../components/Test";
+// import Test from "../components/Test";
 import AboutUs from "../pages/Users/AboutUs/AboutUs";
 import ContactUs from "../pages/Users/Contact/ContactUs";
 import NotFound from "../components/NotFound";
+import OrderDetail from "../pages/Customer/OrderHistory/OrderDetail/OrderDetail";
+import Rating from "../pages/Customer/OrderHistory/Rating/Rating";
+import ServiceList from "../pages/Users/Services/ServiceList";
 const router = createBrowserRouter(
   [
     {
@@ -12,9 +15,11 @@ const router = createBrowserRouter(
       element: <Layout />,
       children: [
         { index: true, element: <HomePage /> },
-        { path: "service", element: <Test /> },
+        { path: "service", element: <ServiceList /> },
         { path: "about", element: <AboutUs /> },
         { path: "contact", element: <ContactUs /> },
+        { path: "orderDetail", element: <OrderDetail /> },
+        { path: "rating", element: <Rating /> },
       ],
     },
     { path: "/*", element: <NotFound /> },
