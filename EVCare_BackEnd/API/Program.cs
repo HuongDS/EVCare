5a977d66646bee8da842455febf0003ca118e30e
slazenger1906 <npsdn1906@gmail.com>
2025-09-25 16:51:19 +0700

    config swagger

--- EVCare_BackEnd/API/Program.cs
+++ EVCare_BackEnd/API/Program.cs
@@ -222,11 +222,13 @@ RecurringJob.AddOrUpdate<IAppointmentExpiryJob>(
        tzVn
     );
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
