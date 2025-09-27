31c997a8592b034784dfe905cafe133ed83bd588
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-09-27 22:00:45 +0700

    fix state login

--- EVCare_FrontEnd/src/pages/Customer/OrderHistory/Rating/Rating.tsx
+++ EVCare_FrontEnd/src/pages/Customer/OrderHistory/Rating/Rating.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Rating from "@mui/material/Rating";
 import NameBox from "../NameBox";
-import CloseButton from "react-bootstrap/CloseButton";
 
 import {
   DetailWrapper,
@@ -19,7 +18,6 @@ import {
   Icon,
   Row,
   LocationBox,
-  MainTitle,
   ModalContent,
 } from "./Rating.styled";
 
