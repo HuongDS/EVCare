dcd062a22d8c3fb287c792acff86db2d285f950e
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-09-28 00:40:42 +0700

    Finished Auth Flow

--- EVCare_FrontEnd/src/services/getServices.ts
+++ EVCare_FrontEnd/src/services/getServices.ts
@@ -1,30 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../api/api";
-import type {
-  ResponseDto,
-  ServicesResponseDto,
-} from "../models/ServicesModel/serviceModel";
+import type { ResponseDto, ServicesResponseDto } from "../models/ServicesModel/serviceModel";
 
 /**
  * Get all active services
  * @returns Object: status code, message, data
  */
-export const getActiveServices = (
-  keyword?: string,
-  payload?: number,
-  pageindex?: number
-) => {
+export const getActiveServices = (keyword?: string, payload?: number, pageindex?: number) => {
   const fetchServiceData = async () => {
-    const response = await api.get<ResponseDto<ServicesResponseDto[]>>(
-      "api/Service/active",
-      {
-        params: { keyword, payload, pageindex },
-      }
-    );
+    const response = await api.get<ResponseDto<ServicesResponseDto[]>>("api/Service/active", {
+      params: { keyword, payload, pageindex },
+    });
     return response.data;
   };
 
   return useQuery({
+    // warning
     queryKey: ["Services", keyword, payload, pageindex],
     queryFn: fetchServiceData,
   });
