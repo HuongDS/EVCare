fcdf5d974dcc86ea7de72c4bbd1eaa465bc69f83
Square <duongnguyenbinhphuong@gmail.com>
2025-09-28 15:46:05 +0700

    Setup: Admin, Technician folder - No Code

---
+++ EVCare_FrontEnd/src/pages/Staff/StaffAppoinments/Staff_Appoinments.tsx
@@ -0,0 +1,41 @@
+import SortTable from "../StaffComponents/SortTable";
+import { AppointmentStatusEnum } from "../../../models/enums";
+import Input from "../StaffComponents/SearchBar";
+import styled from "styled-components";
+
+const AppoimentWrapper = styled.div``;
+
+const TitleWrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h2 {
+    font-weight: 600;
+    color: #4caf50;
+  }
+`;
+
+export default function Staff_Appoinments() {
+  const name = AppointmentStatusEnum;
+  const sortName = [
+    "All",
+    name.PENDING,
+    name.CHECKED_IN,
+    name.CONFIRMED,
+    name.IN_PROGRESS,
+    name.DONE,
+    name.CANCELLED,
+  ];
+  return (
+    <AppoimentWrapper>
+      <TitleWrapper>
+        <h2>Appoinments</h2>
+        <Input />
+      </TitleWrapper>
+      <SortTable sortName={sortName} />
+      <div></div>
+    </AppoimentWrapper>
+  );
+}
