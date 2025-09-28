96b06a653704980447946f50b5dc0059ccbfd353
vyngochi <ncv.ngovy@gmail.com>
2025-09-28 12:51:32 +0700

    Fix: routes

--- EVCare_FrontEnd/src/utils/router.tsx
+++ EVCare_FrontEnd/src/utils/router.tsx
@@ -1,67 +1,47 @@
 import { createBrowserRouter } from "react-router-dom";
-import CustomerLayout from "../components/Layouts/CustomerLayout";
-import AdminLayout from "../components/Layouts/AdminLayout";
-import TechnicianLayout from "../components/Layouts/TechnicianLayout";
-import StaffLayout from "../components/Layouts/StaffLayout";
-
-import HomePage from "../pages/Users/HomePage/HomePage";
-import ServiceList from "../pages/Users/Services/ServiceList";
+import HomePage from "../../src/pages/Users/HomePage/HomePage";
+import Layout from "../../src/components/Layouts/CustomerLayout";
 import AboutUs from "../pages/Users/AboutUs/AboutUs";
 import ContactUs from "../pages/Users/Contact/ContactUs";
 import OrderDetail from "../pages/Customer/OrderHistory/OrderDetail/OrderDetail";
 import Rating from "../pages/Customer/OrderHistory/Rating/Rating";
-import Test from "../components/Test";
-
 import PageNotFound from "../components/Layouts/PageNotFound";
-import AdminGeneral from "../pages/Admin/General";
-
-import StaffGeneral from "../pages/Staff/StaffSections/Staff_General";
-import StaffInventory from "../pages/Staff/StaffSections/Staff_Inventory";
-import Staff_Appoinments from "../pages/Staff/StaffSections/Staff_Appoinments";
-
-import TechnicianGeneral from "../pages/Technician/General";
+import Test from "../components/Test";
+import ServiceList from "../pages/Users/Services/ServiceList";
+import StaffLayout from "../pages/Staff/StaffLayout";
 const router = createBrowserRouter([
-  // Customer routes
   {
     path: "/",
-    element: <CustomerLayout />,
+    element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "service", element: <ServiceList /> },
+      {
+        path: "service",
+        element: <ServiceList />,
+      },
       { path: "about", element: <AboutUs /> },
-      { path: "contact", element: <ContactUs /> },
-      { path: "orderDetail", element: <OrderDetail /> },
+      {
+        path: "contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "orderDetail",
+        element: <OrderDetail />,
+      },
       { path: "rating", element: <Rating /> },
+      { path: "test", element: <Test /> },
     ],
   },
-
-  // Admin routes
   {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [{ path: "general", element: <AdminGeneral /> }],
-  },
-
-  // Staff routes
-  {
-    path: "/staff",
+    path: "staff",
     element: <StaffLayout />,
     children: [
-      { path: "general", element: <StaffGeneral /> },
-      { path: "inventory", element: <StaffInventory /> },
-      { path: "appointments", element: <Staff_Appoinments /> },
+      {
+        path: "appointments",
+        element: <Staff_Appoinments />,
+      },
     ],
   },
-
-  // Technician routes
-  {
-    path: "/technician",
-    element: <TechnicianLayout />,
-    children: [{ path: "tasks", element: <TechnicianGeneral /> }],
-  },
-
-  // Test route
-  { path: "/test", element: <Test /> },
   { path: "/*", element: <PageNotFound /> },
 ]);
 
