4389a46e9e4b10345a568095486887c9bd9021ef
slazenger1906 <npsdn1906@gmail.com>
2025-09-30 18:14:37 +0700

    create service center controller

--- EVCare_BackEnd/Application/Mappings/ServiceCenterProfile.cs
+++ EVCare_BackEnd/Application/Mappings/ServiceCenterProfile.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using DataAccess.Dtos.Others;
+using DataAccess.Dtos.Service;
+using DataAccess.Dtos.ServiceCenter;
 using DataAccess.Entities;
 
 namespace Application.Mappings
@@ -18,6 +20,29 @@ namespace Application.Mappings
                 opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.centerAddress, opt =>
                 opt.MapFrom(src => src.Address));
+
+            CreateMap<ServiceCenterViewModel, ServiceCenter>()
+                .ForMember(dest => dest.Name, opt =>
+                {
+                    opt.PreCondition(src => !string.IsNullOrEmpty(src.Name));
+                })
+                .ForMember(dest => dest.Address, opt =>
+                {
+                    opt.PreCondition(src => !string.IsNullOrEmpty(src.Address));
+                })
+                .ForMember(dest => dest.OpenTime, opt =>
+                {
+                    opt.PreCondition(src => src.OpenTime != null);
+                })
+                .ForMember(dest => dest.CloseTime, opt =>
+                {
+                    opt.PreCondition(src => src.CloseTime != null);
+                })
+                .ForMember(dest => dest.Hotline, opt =>
+                {
+                    opt.PreCondition(src => !string.IsNullOrEmpty(src.Hotline));
+                })
+                .ReverseMap();
         }
     }
 }
