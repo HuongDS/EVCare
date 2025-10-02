c44b62240f545f635ed04a31ced3b18572f34f35
slazenger1906 <npsdn1906@gmail.com>
2025-10-02 13:58:53 +0700

    add technicianworkingsession controller

--- EVCare_BackEnd/Application/Interfaces/ITechnicianWorkingSessionService.cs
+++ EVCare_BackEnd/Application/Interfaces/ITechnicianWorkingSessionService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Dtos.Technician;
 
 namespace Application.Interfaces
 {
     public interface ITechnicianWorkingSessionService
     {
-        
+        Task UpdateWorkingSession(int technicianId,TechnicianWorkingSessionUpdateModel model);
     }
 }
