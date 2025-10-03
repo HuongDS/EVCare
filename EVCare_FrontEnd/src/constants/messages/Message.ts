4d8a4fb4430319fccb88c361ab5f1aeb38e6c85a
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-03 14:20:07 +0700

    booking form

--- EVCare_FrontEnd/src/constants/messages/Message.ts
+++ EVCare_FrontEnd/src/constants/messages/Message.ts
@@ -14,6 +14,9 @@ export const ERROR_MESSAGE = {
   CREATE_APPOINTMENT_FAILED: "Failed to create an appointment.",
   CREATE_VEHICLE_FAILED: "Failed to create a vehicle.",
   RESET_PASSWORD_FAILED: "Failed to reset password",
+  LICENSE_PLATE_WRONG: "License format was wrong format. Please try again!",
+  SERVICES_MUST_NOT_BE_EMPTY: "Please choose at least 1 service!",
+  DATE_AND_TIME_CAN_NOT_BE_EMPTY: "Please select a suitable day and time!",
 };
 
 export const SUCCESS_MESSAGE = {
