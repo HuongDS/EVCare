cabbeb0faa6ce9eca189be3f5a0556466d12c475
slazenger1906 <npsdn1906@gmail.com>
2025-10-05 00:28:33 +0700

    update

---
+++ EVCare_BackEnd/API/Middlewares/RateLimitMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace API.Middlewares
+{
+    public class RateLimitMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private static readonly object _gate = new();
+        private static readonly Stopwatch _sw = Stopwatch.StartNew();
+        private static int _count = 0;
+
+        private readonly int _limit;
+        private readonly TimeSpan _window;
+        public RateLimitMiddleware(RequestDelegate next, int limit = 50, double seconds = 1.0)
+        {
+            _next = next;
+            _limit = limit;
+            _window = TimeSpan.FromSeconds(seconds);
+        }
+    }
+}
