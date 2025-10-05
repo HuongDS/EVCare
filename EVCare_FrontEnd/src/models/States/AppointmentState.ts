b4508982208be1f60dd0c39e839d303778d4f59c
vyngochi <ncv.ngovy@gmail.com>
2025-10-06 02:16:48 +0700

    Feature: Staff manage appoinments - continuing

--- EVCare_FrontEnd/src/models/States/AppointmentState.ts
+++ EVCare_FrontEnd/src/models/States/AppointmentState.ts
@@ -1,11 +1,3 @@
-import type { StaffAppointmentsDto } from "../AppointmentsModel/Staff_Appointments_Model";
-
-export interface AppointmentState {
-  items: StaffAppointmentsDto[];
-  pageSize: number;
-  pageIndex: number;
-  totalItems: number;
-  totalPages: number;
-  loading: boolean;
-  error?: string | null;
+export interface AppointmentProgressState {
+  [appointmentID: number]: number;
 }
