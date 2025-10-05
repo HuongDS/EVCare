0c7351681f84d1f64a9541fe6426a860bb361737
slazenger1906 <npsdn1906@gmail.com>
2025-10-05 22:52:31 +0700

    add update order part for technician (1)

--- EVCare_BackEnd/DataAccess/Repositories/TechnicianWorkingSessionRepository.cs
+++ EVCare_BackEnd/DataAccess/Repositories/TechnicianWorkingSessionRepository.cs
@@ -31,6 +31,19 @@ namespace DataAccess.Repositories
             return !anyComplete;
         }
 
+        public async Task<TechnicianWorkingSessionViewModel> GetTechnicianWorkingSession(int orderId, int technicianId)
+        {
+            return await _dbContext.TechnicianWorkingSessions.AsNoTracking()
+                .Select(x => new TechnicianWorkingSessionViewModel
+                {
+                    OrderId = x.OrderId,
+                    Status = x.Status,
+                    TechnicianId = x.TechnicianId,
+                })
+                .FirstOrDefaultAsync(x => x.OrderId == orderId && technicianId == x.TechnicianId);
+
+        }
+
         public async Task UpdateStatusWorkingSession(int technician, TechnicianWorkingSessionUpdateModel model)
         {
             var data = await _dbContext.TechnicianWorkingSessions.Where(x=>x.TechnicianId == technician && x.OrderId == model.OrderId).FirstOrDefaultAsync();
