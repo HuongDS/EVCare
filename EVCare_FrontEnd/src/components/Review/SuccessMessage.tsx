b27f66cfcde3478ccc440a82db97c37ca5969b01
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-09 00:31:45 +0700

    Implement Text Editor

--- EVCare_FrontEnd/src/components/Review/SuccessMessage.tsx
+++ EVCare_FrontEnd/src/components/Review/SuccessMessage.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import {ReviewWrapper} from "./Review.styled.tsx";
 
 interface SuccessMessageProps {
-  onClose: () => void;
+    onClose: () => void;
 }
 
-const SuccessMessage: React.FC<SuccessMessageProps> = ({ onClose }) => {
-  return (
-    <div className="success-message active">
-      <div className="success-icon">✓</div>
-      <h3 className="success-title">Review Submitted</h3>
-      <p className="success-text">Thank you for sharing your feedback with us.</p>
-      <button className="submit-btn" style={{ marginTop: 30, maxWidth: 200 }} onClick={onClose}>
-        Close
-      </button>
-    </div>
-  );
+const SuccessMessage: React.FC<SuccessMessageProps> = ({onClose}) => {
+    return (
+        <ReviewWrapper>
+            <div className="success-message active">
+                <div className="success-icon">✓</div>
+                <h3 className="success-title">Review Submitted</h3>
+                <p className="success-text">Thank you for sharing your feedback with us.</p>
+                <button className="submit-btn" style={{marginTop: 30, maxWidth: 200}} onClick={onClose}>
+                    Close
+                </button>
+            </div>
+        </ReviewWrapper>
+    );
 };
 
 export default SuccessMessage;
