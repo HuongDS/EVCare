22476ffb31468bbad3967e9f8c82f8ecbee4e327
slazenger1906 <npsdn1906@gmail.com>
2025-10-09 22:36:33 +0700

    update relation for review

--- EVCare_BackEnd/DataAccess/Migrations/20251009100027_deleteRealtion1-n.Designer.cs
+++ EVCare_BackEnd/DataAccess/Migrations/20251009100027_deleteRealtion1-n.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(EVCareDbContext))]
-    partial class EVCareDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251009100027_deleteRealtion1-n")]
+    partial class deleteRealtion1n
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
