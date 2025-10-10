f4a26e00d838bef7d12a43901ec4a5d978805645
slazenger1906 <npsdn1906@gmail.com>
2025-10-10 21:36:24 +0700

    fix conflit

--- EVCare_BackEnd/Application/Interfaces/IAppointmentService.cs
+++ EVCare_BackEnd/Application/Interfaces/IAppointmentService.cs
@@ -32,7 +32,6 @@ namespace Application.Interfaces
         Task<CenterDailyCapacityModel> GetAppointmentWithCountDaily();
         Task <PageResultDto<AppointmentTechnicianViewModel>> GetAppointmentByTechnicianId(int technicianId, AppointmentTechnicianQueryDto model);
         Task<PageResultDto<AppointmentInProgressUnderstaffedViewModel>> GetUnderstaffedInProgressAsync(AppointmentQueryDto model);
-        Task<PageResultDto<AppointmentTechnicianViewModel>> GetAppointmentByTechnicianId(int technicianId, AppointmentTechnicianQueryDto model);
         Task<int> CountAppointmentsInMonths(int year, int month);
         Task<int> CountCustomersInMonths(int year, int month);
         Task<int> CountAppointmentsInMonthsWithStatus(int year, int month, AppointmentStatusEnum status);
