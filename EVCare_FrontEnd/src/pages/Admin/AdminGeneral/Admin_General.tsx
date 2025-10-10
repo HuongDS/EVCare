7e2b2a827c0766852abdc961dde5e3cde90c3d5d
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-10 23:19:07 +0700

    Implement Ai in AdminDashboard

--- EVCare_FrontEnd/src/pages/Admin/AdminGeneral/Admin_General.tsx
+++ EVCare_FrontEnd/src/pages/Admin/AdminGeneral/Admin_General.tsx
@@ -5,23 +5,19 @@ import InvoicesTable from "./InvoicesTable.tsx";
 import { useEffect, useState } from "react";
 import { useAlert } from "../../../context/useAlert.ts";
 import { MSG_TITLE } from "../../../constants/messages/Message.ts";
-import { getInvoicesWithPagination, getRevenue } from "../../../services/invoicesService.ts";
+import { getInvoicesWithPagination } from "../../../services/invoicesService.ts";
 import HTTP_STATUS from "../../../constants/Code/HttpStatusCode.ts";
-import {
-  countAppointmentsInMonth,
-  countAppointmentsWithStatusInMonth,
-  countCustomersInMonth,
-} from "../../../services/appointmentServiceApi.ts";
-import { AppointmentStatusEnum } from "../../../models/enums/AppointmentStatusEnum.tsx";
+// import {
+//   countAppointmentsInMonth,
+//   countAppointmentsWithStatusInMonth,
+//   countCustomersInMonth,
+// } from "../../../services/appointmentServiceApi.ts";
+// import { AppointmentStatusEnum } from "../../../models/enums/AppointmentStatusEnum.tsx";
 import type { InvoiceViewModel } from "../../../models/Invoice/InvoiceViewModel.tsx";
 import SpinnerComponent from "../../../components/SpinnerComponent.tsx";
 import LazyPerformanceChart from "./LazyPerformanceChart.tsx";
 
 export default function Admin_General() {
-  // const [revenue, setRevenue] = useState(0);
-  // const [numberOfCustomers, setNumberOfCustomers] = useState(0);
-  // const [numberOfAppointments, setNumberOfAppointments] = useState(0);
-  // const [numberOfCanceledAppointments, setNumberOfCanceledAppointments] = useState(0);
   const [listInvoices, setListInvoices] = useState<InvoiceViewModel[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -31,40 +27,11 @@ export default function Admin_General() {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const dateNow = new Date();
-        const response01 = await getRevenue(dateNow.getFullYear(), dateNow.getMonth() + 1);
-        if (response01.statusCode !== HTTP_STATUS.OK) {
-          throw new Error(response01.message);
+        const response = await getInvoicesWithPagination(10, 1);
+        if (response.statusCode !== HTTP_STATUS.OK) {
+          throw new Error(response.message);
         }
-        // setRevenue(response01.data ?? 0);
-
-        const response02 = await countAppointmentsInMonth(dateNow.getFullYear(), dateNow.getMonth());
-        if (response02.statusCode !== HTTP_STATUS.OK) {
-          throw new Error(response01.message);
-        }
-        // setNumberOfAppointments(response02.data ?? 0);
-
-        const response03 = await countAppointmentsWithStatusInMonth(
-          dateNow.getFullYear(),
-          dateNow.getMonth(),
-          AppointmentStatusEnum.CANCELED
-        );
-        if (response03.statusCode !== HTTP_STATUS.OK) {
-          throw new Error(response01.message);
-        }
-        // setNumberOfCanceledAppointments(response03.data ?? 0);
-
-        const response04 = await countCustomersInMonth(dateNow.getFullYear(), dateNow.getMonth());
-        if (response04.statusCode !== HTTP_STATUS.OK) {
-          throw new Error(response01.message);
-        }
-        // setNumberOfCustomers(response04.data ?? 0);
-
-        const response05 = await getInvoicesWithPagination(10, 1);
-        if (response05.statusCode !== HTTP_STATUS.OK) {
-          throw new Error(response01.message);
-        }
-        setListInvoices(response05.data?.items ?? []);
+        setListInvoices(response.data?.items ?? []);
       } catch (ex) {
         showAlert("error", MSG_TITLE.ADMIN, ex as string);
       } finally {
