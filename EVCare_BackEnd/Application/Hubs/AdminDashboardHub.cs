7b6f9f70863089bb571105053032f95c2c38c4da
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-11 00:39:30 +0700

    test hub signalR_01

--- EVCare_BackEnd/Application/Hubs/AdminDashboardHub.cs
+++ EVCare_BackEnd/Application/Hubs/AdminDashboardHub.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace API.Hubs
 {
+    [AllowAnonymous]
     public class AdminDashboardHub : Hub
     {
     }
