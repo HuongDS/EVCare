23000ad1ee64de598b20f1876c55312d69fe212d
slazenger1906 <npsdn1906@gmail.com>
2025-10-11 20:22:10 +0700

    update applictaion controller

--- EVCare_BackEnd/Application/Interfaces/IApplicationServices.cs
+++ EVCare_BackEnd/Application/Interfaces/IApplicationServices.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Application.Dtos;
 using DataAccess.Dtos.Applications;
+using DataAccess.Dtos.Pagination;
 
 namespace Application.Interfaces
 {
     public interface IApplicationServices
     {
+        Task<PageResultDto<ApplicationViewDto>> GetApplicationAsync(ApplicationQueryDto query, int employeeId);
         Task<ResponseDto<ApplicationViewDto>> SendApplicationAsync(ApplicationCreateDto data);
     }
 }
