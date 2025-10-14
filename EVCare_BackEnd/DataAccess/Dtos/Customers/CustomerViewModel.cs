4d984c158e0b2ff4c808584c9713a6e10b325641
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-14 13:26:20 +0700

    fix search

--- EVCare_BackEnd/DataAccess/Dtos/Customers/CustomerViewModel.cs
+++ EVCare_BackEnd/DataAccess/Dtos/Customers/CustomerViewModel.cs
@@ -11,10 +11,10 @@ namespace DataAccess.Dtos.Customers
     {
         public int AccountId { get; set; }
         public string CustomerName { get; set; }
-
+        public bool Banned { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; } 
-        public string Address { get; set; } 
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
         public IEnumerable<VehicleViewModel> Vehicles { get; set; }
 
     }
