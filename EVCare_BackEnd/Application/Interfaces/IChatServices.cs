a9353508306a288549a01ab3352cadea6b0981b0
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-17 16:04:00 +0700

    fix message

--- EVCare_BackEnd/Application/Interfaces/IChatServices.cs
+++ EVCare_BackEnd/Application/Interfaces/IChatServices.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Dtos.MongoDb_Message;
 
 namespace Application.Interfaces
 {
     public interface IChatServices
     {
+        Task<List<Message>> GetHistoryAsync(int conversationId, int skip, int take);
+        Task MarkAsReadUpToAsync(int conversationId, int readerId, int upToMessageId);
+        Task<Message> SaveMessageAsync(int conversationId, int senderId, string text, List<Attachment> atts);
     }
 }
