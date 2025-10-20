336f7174eff03251a93a25472072f32d79619954
slazenger1906 <npsdn1906@gmail.com>
2025-10-20 20:37:07 +0700

    add part history table

--- EVCare_BackEnd/DataAccess/Migrations/20251020090736_createActionTypeForPartHistories.Designer.cs
+++ EVCare_BackEnd/DataAccess/Migrations/20251020090736_createActionTypeForPartHistories.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(EVCareDbContext))]
-    partial class EVCareDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251020090736_createActionTypeForPartHistories")]
+    partial class createActionTypeForPartHistories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -880,9 +883,8 @@ namespace DataAccess.Migrations
                     b.Property<DateTime>("ChangeDate")
                         .HasColumnType("datetime2");
 
-                    b.Property<string>("EmployeeName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("EmployeeId")
+                        .HasColumnType("int");
 
                     b.Property<int>("NewQuantity")
                         .HasColumnType("int");
---
+++ EVCare_BackEnd/DataAccess/Repositories/PartHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Entities;
+using DataAccess.Interfaces;
+
+namespace DataAccess.Repositories {
+    public class PartHistoryRepository : GenericRepository<PartHistory>, IPartHistoryRepository {
+        public PartHistoryRepository(EVCareDbContext dbContext) : base(dbContext) {
+        }
+
+    }
+}
