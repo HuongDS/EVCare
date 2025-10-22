5215e2791255c14b591295c94b724dae85f2476d
slazenger1906 <npsdn1906@gmail.com>
2025-10-22 23:48:18 +0700

    create repository service controller for appointment part condition

---
+++ EVCare_BackEnd/API/Controllers/AppointmentPartConditionController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentPartConditionController : ControllerBase {
+       private readonly IAppointmentPartConditionService _appointmentPartConditionService;
+         public AppointmentPartConditionController(IAppointmentPartConditionService appointmentPartConditionService) {
+              _appointmentPartConditionService = appointmentPartConditionService;
+        }
+
+    }
+}
