d0d3836b64b72ffdeb3e526df386f3b210b1a934
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-24 14:36:38 +0700

    fix props in appointment repository

--- EVCare_BackEnd/DataAccess/Repositories/AppointmentRepository.cs
+++ EVCare_BackEnd/DataAccess/Repositories/AppointmentRepository.cs
@@ -122,7 +122,7 @@ namespace DataAccess.Repositories
                     AppointmentImages = a.AppointmentImages.Select(x => x.Image).ToList(),
                     CustomerName = a.Customer.Account.First_Name + " " + a.Customer.Account.Last_Name,
                     PhoneNumber = a.Customer.Account.Phone,
-                    
+
                 }).Where(x => x.CustomerName.Contains(customername));
 
             return await PaginationHelper.PaginationAsync(query, payload, pageindex);
@@ -148,7 +148,7 @@ namespace DataAccess.Repositories
                     VehiclePlateNumber = a.Vehicle.LicensePlate,
                     CustomerName = a.Customer.Account.First_Name + " " + a.Customer.Account.Last_Name,
                     CustomerEmail = a.Customer.Account.Email,
-                    CustomerPhone = a.Customer.Account.Phone,
+                    PhoneNumber = a.Customer.Account.Phone,
                     EmployeeName = a.Employee != null
                         ? a.Employee.Account.First_Name + " " + a.Employee.Account.Last_Name
                         : null,
@@ -302,8 +302,9 @@ namespace DataAccess.Repositories
         //    return await PaginationHelper.PaginationAsync(query, model.PageSize.Value, model.PageIndex.Value);
 
         //}
-        public async Task<PageResultDto<AppointmentViewModel>> GetWithPaginationAsync(AppointmentQueryDto model) {
-        
+        public async Task<PageResultDto<AppointmentViewModel>> GetWithPaginationAsync(AppointmentQueryDto model)
+        {
+
             var baseQuery = _dbSet
                 .AsNoTracking()
                 .Where(a =>
@@ -322,7 +323,7 @@ namespace DataAccess.Repositories
                     CustomerName = a.Customer.Account.First_Name + " " + a.Customer.Account.Last_Name
                 });
 
-         
+
             baseQuery = baseQuery.ApplySorting(model.SortField, model.SortOrder);
 
             var pagedResult = await PaginationHelper.PaginationAsync(baseQuery, model.PageSize.Value, model.PageIndex.Value);
@@ -331,7 +332,7 @@ namespace DataAccess.Repositories
 
             var appointmentIds = pagedResult.Items.Select(x => x.Id).ToList();
 
-         
+
             var appointments = await _dbSet
                 .AsNoTracking()
                 .Where(a => appointmentIds.Contains(a.Id))
@@ -385,13 +386,13 @@ namespace DataAccess.Repositories
                 })
                 .ToListAsync();
 
-          
+
             var orderedAppointments = appointmentIds
                 .Select(id => appointments.FirstOrDefault(a => a.Id == id))
                 .Where(a => a != null)
                 .ToList();
 
-          
+
             return new PageResultDto<AppointmentViewModel>(
                 orderedAppointments,
                 pagedResult.TotalItems,
@@ -405,7 +406,7 @@ namespace DataAccess.Repositories
             var start = model.BeginTime?.ToDateTime(TimeOnly.MinValue);
             var end = model.EndTime?.ToDateTime(TimeOnly.MaxValue);
 
-           
+
             var baseQuery = _dbContext.TechnicianWorkingSessions
                 .AsNoTracking()
                 .Where(s =>
@@ -414,14 +415,17 @@ namespace DataAccess.Repositories
                     (!end.HasValue || s.Order.Appointment.Appointment_Date <= end.Value)
                 );
 
-            if (model.Status.HasValue) {
-                if (model.Status == TechnicianWorkingSessionEnum.Completed) {
+            if (model.Status.HasValue)
+            {
+                if (model.Status == TechnicianWorkingSessionEnum.Completed)
+                {
                     baseQuery = baseQuery.Where(s => s.Status == TechnicianWorkingSessionEnum.Completed);
                 }
-                else {
+                else
+                {
                     baseQuery = baseQuery
                         .Where(s => s.Status == model.Status.Value)
-                        .OrderByDescending(s => s.Order.Appointment.Appointment_Date) 
+                        .OrderByDescending(s => s.Order.Appointment.Appointment_Date)
                         .Take(1);
                 }
             }
@@ -522,12 +526,12 @@ namespace DataAccess.Repositories
         }
         public async Task<PageResultDto<AppointmentInProgressUnderstaffedViewModel>> GetUnderstaffedInProgressAsync(AppointmentQueryDto model)
         {
-            var query =  _dbContext.Appointments.AsNoTracking()
+            var query = _dbContext.Appointments.AsNoTracking()
                 .Where(x => x.Status == AppointmentStatusEnum.InProgress)
                 .Select(x => new AppointmentInProgressUnderstaffedViewModel
                 {
                     AppointmentDate = x.Appointment_Date,
-                    CustomerName = x.Customer.Account.First_Name+" " + x.Customer.Account.First_Name,
+                    CustomerName = x.Customer.Account.First_Name + " " + x.Customer.Account.First_Name,
                     CustomerPhone = x.Customer.Account.Phone,
                     Id = x.Id,
                     CustomerEmail = x.Customer.Account.Email,
@@ -538,7 +542,7 @@ namespace DataAccess.Repositories
                     }).ToList(),
                     VehicleName = x.Vehicle.Category.Name,
                     VehiclePlateNumber = x.Vehicle.LicensePlate,
-                    Technicians= x.Order.TechnicianWorkingSessions
+                    Technicians = x.Order.TechnicianWorkingSessions
                                 .Select(t => new TechnicianViewModel
                                 {
                                     Id = t.TechnicianId,
@@ -546,7 +550,7 @@ namespace DataAccess.Repositories
                                     FullName = t.Technician.Employee.Account.First_Name + " " + t.Technician.Employee.Account.Last_Name,
                                     Phone = t.Technician.Employee.Account.Phone,
                                     Skills = t.Technician.TechnicianSkills
-                                                .Select(ts =>new ServiceViewFormModel
+                                                .Select(ts => new ServiceViewFormModel
                                                 {
                                                     Id = ts.ServiceId,
                                                     Name = ts.Service.Name
@@ -556,12 +560,12 @@ namespace DataAccess.Repositories
                                 }).ToList()
 
                 })
-                .Where(x=>x.Technicians.Any(x=>x.Status == EmployeeStatusEnum.OnLeave))
+                .Where(x => x.Technicians.Any(x => x.Status == EmployeeStatusEnum.OnLeave))
                 .ApplySorting(model.SortField, model.SortOrder)
                 ;
-           return await PaginationHelper.PaginationAsync(query,model.PageSize.Value,model.PageIndex.Value);
-          }
-          
+            return await PaginationHelper.PaginationAsync(query, model.PageSize.Value, model.PageIndex.Value);
+        }
+
         public async Task<int> CountAppointmentsInMonth(int year, int month)
         {
             var startDate = new DateTime(year, month, 1);
@@ -580,9 +584,9 @@ namespace DataAccess.Repositories
             var endDate = startDate.AddMonths(1);
             return await _dbContext
             .Appointments
-            .Where(a => a.Status != AppointmentStatusEnum.Pending 
+            .Where(a => a.Status != AppointmentStatusEnum.Pending
             && a.Status != AppointmentStatusEnum.Canceled
-            && a.Appointment_Date >= startDate 
+            && a.Appointment_Date >= startDate
             && a.Appointment_Date <= endDate)
             .GroupBy(a => a.CustomerId)
             .CountAsync();
