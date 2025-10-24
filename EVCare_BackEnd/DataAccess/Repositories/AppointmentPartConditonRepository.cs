c6bb50a2f6f6b9a6234f45a06e2803e1e381a26c
slazenger1906 <npsdn1906@gmail.com>
2025-10-24 12:46:21 +0700

    create api for appointment part condition

--- EVCare_BackEnd/DataAccess/Repositories/AppointmentPartConditonRepository.cs
+++ EVCare_BackEnd/DataAccess/Repositories/AppointmentPartConditonRepository.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Dtos.AppointmentPartCondition;
 using DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories {
     public class AppointmentPartConditonRepository : IAppointmentPartConditionRepository {
@@ -11,6 +13,32 @@ namespace DataAccess.Repositories {
         public AppointmentPartConditonRepository(EVCareDbContext dbcontext) {
             _dbcontext = dbcontext;
         }
+        public async Task CreateAppointmentPartConditionAsync(Entities.AppointmentPartCondition appointmentPartCondition) {
+            await _dbcontext.AppointmentPartConditions.AddAsync(appointmentPartCondition);
+        }
 
+        public async Task DeleteAppointmentPartConditionsByAppointmentIdAsync(int appointmentId, int technicianId) {
+            await  _dbcontext.AppointmentPartConditions
+                .Where(apc => apc.AppointmentId == appointmentId && apc.TechicianId == technicianId)
+                .ExecuteDeleteAsync();
+        }
+
+        public async Task<AppointmentPartConditionViewModel> GetAppointmentPartConditionsAsync(int appointmentId, int technicianId) {
+            var parts = await _dbcontext.AppointmentPartConditions.
+                Where(a => a.AppointmentId == appointmentId && a.TechicianId == technicianId)
+                .Include(a => a.Part)
+                .Select(a => new AppointmentPartDamageLevelViewModel {
+                    PartId = a.PartId,
+                    DamageLevel = a.Level,
+                    PartName = a.Part.Name,
+                    PartUrl = a.Part.Image
+
+                }).ToListAsync();
+                    return new AppointmentPartConditionViewModel
+                    {
+                        AppointmentId = appointmentId,
+                        PartDamageLevels = parts
+                    };
+           }
     }
 }
