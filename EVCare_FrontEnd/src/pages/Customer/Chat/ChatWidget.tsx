3319045b79b9b9fed8a42e2a0223be6e08a50c4f
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-10-31 20:30:34 +0700

    fix chat button

--- EVCare_FrontEnd/src/pages/Customer/Chat/ChatWidget.tsx
+++ EVCare_FrontEnd/src/pages/Customer/Chat/ChatWidget.tsx
@@ -3,21 +3,27 @@ import { Button } from "antd";
 import { MessageOutlined, CloseOutlined } from "@ant-design/icons";
 import { ChatPage } from "./ChatPage";
 import { WidgetChatStyleWrapper } from "./WidgetChat.styled";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../states/store";
+import { RoleEnum } from "../../../models/enums";
 
 export const ChatWidget: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const role = useSelector((state: RootState) => state.auth.user?.role);
 
   const toggleChat = () => setIsOpen(!isOpen);
 
   return (
     <WidgetChatStyleWrapper>
-      <Button
-        className="chat-widget-trigger"
-        type="primary"
-        shape="circle"
-        icon={isOpen ? <CloseOutlined /> : <MessageOutlined />}
-        onClick={toggleChat}
-      />
+      {role === RoleEnum.CUSTOMER && (
+        <Button
+          className="chat-widget-trigger"
+          type="primary"
+          shape="circle"
+          icon={isOpen ? <CloseOutlined /> : <MessageOutlined />}
+          onClick={toggleChat}
+        />
+      )}
 
       {isOpen && (
         <div className="chat-widget-container">
