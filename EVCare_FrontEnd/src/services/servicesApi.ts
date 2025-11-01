325b9bdab1530311c37569f84a3d8b0574d992d8
vyngochi <ncv.ngovy@gmail.com>
2025-11-02 02:38:57 +0700

    Function: add react-query hook for get all services

--- EVCare_FrontEnd/src/services/servicesApi.ts
+++ EVCare_FrontEnd/src/services/servicesApi.ts
@@ -5,6 +5,10 @@ import type {
   ServicePageModel,
   ServicesResponseDto,
 } from "../models/ServicesModel/Customer_Services_Model";
+import type { GetServiceParams } from "../models/ServicesModel/ServiceViewModel";
+import { handleError } from "../utils/errorHandler";
+import axios from "axios";
+import { ERROR_MESSAGE } from "../constants/messages/Message";
 
 /**
  * [STAFF, CUSTOMER, ADMIN] - get all services
@@ -50,5 +54,31 @@ export const getAllActiveService = (
     queryKey: ["Services", keyword, pageSize, pageIndex, sortField, sortOrder],
     queryFn: () =>
       fetchServiceData(keyword, pageSize, pageIndex, sortField, sortOrder),
+  });
+};
+
+export const useGetAllServices = (params: GetServiceParams) => {
+  return useQuery({
+    queryKey: ["Services", params],
+    queryFn: async () => {
+      try {
+        const response = await api.get<
+          ResponseDto<ServicePageModel<ServicesResponseDto>>
+        >("api/Service/active", {
+          params,
+        });
+        return response.data;
+      } catch (error) {
+        handleError(error);
+        if (axios.isAxiosError(error)) {
+          const errMsg =
+            error.response?.data?.message ||
+            error.message ||
+            ERROR_MESSAGE.FETCH_DATA_FAILED;
+          throw new Error(errMsg);
+        }
+        throw new Error(ERROR_MESSAGE.SOME_THING_WENT_WRONG);
+      }
+    },
   });
 };
