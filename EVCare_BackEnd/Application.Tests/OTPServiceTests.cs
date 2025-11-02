3df46f3569533931736ee75d926d15f9d3d72b9e
slazenger1906 <npsdn1906@gmail.com>
2025-11-02 21:13:28 +0700

    done test for save otp async and verify otp async

--- EVCare_BackEnd/Application.Tests/OTPServiceTests.cs
+++ EVCare_BackEnd/Application.Tests/OTPServiceTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Services;
+using AutoFixture.Xunit2;
 using DataAccess.Entities;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Moq;
@@ -31,11 +32,50 @@ namespace Application.Tests {
 
             // Assert
             Assert.True(result);
-            
+        }
+        [Theory,AutoData]
+        public async Task VerifyOtpAsync_WithInvalidEmailAndOtp_ReturnsFasle(string email,string otp) {
+            var dbMock = new Mock<IDatabase>();
+            dbMock.Setup(db => db.StringGetAsync(It.IsAny<RedisKey>(), It.IsAny<CommandFlags>()))
+             .ReturnsAsync((string?)null);
+            var connectionMock = new Mock<IConnectionMultiplexer>();
+            connectionMock.Setup(c => c.GetDatabase(It.IsAny<int>(), It.IsAny<object>()))
+                          .Returns(dbMock.Object);
+            var otpService = new OtpServices(connectionMock.Object);
+            var result = await otpService.VerifyOtpAsync(email, otp);
+            Assert.False(result);
 
 
+        }
 
 
+        [Theory,AutoData]
+        public async Task SaveOtpAsync_WithValidData_SavesOtpToDatabase(string email, string otp, int expireMinutes) {
+         
+            var dbMock = new Mock<IDatabase>();
+            dbMock.Setup(db => db.StringSetAsync(
+                        It.IsAny<RedisKey>(),
+                        It.IsAny<RedisValue>(),
+                        It.IsAny<TimeSpan?>(),
+                        It.IsAny<bool>(),         
+                        It.IsAny<When>(),
+                        It.IsAny<CommandFlags>()))
+                    .ReturnsAsync(true);
+            var connectionMock = new Mock<IConnectionMultiplexer>();
+            connectionMock.Setup(c => c.GetDatabase(It.IsAny<int>(), It.IsAny<object>()))
+                          .Returns(dbMock.Object);
+            var otpService = new OtpServices(connectionMock.Object);
+          
+            await otpService.SaveOtpAsync(email, otp, expireMinutes);
+       
+            dbMock.Verify(db => db.StringSetAsync(
+                 It.Is<RedisKey>(k => k.ToString() == $"otp:{email}"),
+                 It.Is<RedisValue>(v => v.ToString() == otp),
+                 It.IsAny<TimeSpan?>(),
+                 It.Is<bool>(b => b == false),   
+                 It.Is<When>(w => w == When.Always),
+                 It.Is<CommandFlags>(f => f == CommandFlags.None)),
+             Times.Once);
         }
     }
 
