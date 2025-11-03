4a48f696d0117a5c8288b2ddce418705e3a27ff0
slazenger1906 <npsdn1906@gmail.com>
2025-11-03 13:12:09 +0700

    update scale for api

--- EVCare_BackEnd/DataAccess/Dtos/VehicleCategory/VehicleCategoryCreateModel.cs
+++ EVCare_BackEnd/DataAccess/Dtos/VehicleCategory/VehicleCategoryCreateModel.cs
@@ -8,6 +8,9 @@ namespace DataAccess.Dtos.VehicleCategory {
     public class VehicleCategoryCreateModel {
         public string Name { get; set; }
         public string? Model3DUrl { get; set; }
+        public decimal? ScaleX { get; set; }
+        public decimal? ScaleY { get; set; }
+        public decimal? ScaleZ { get; set; }
         public int[] PartCategoryIds { get; set; }
     }
 }
