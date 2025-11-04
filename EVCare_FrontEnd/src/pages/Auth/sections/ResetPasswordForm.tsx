63a880822895c6ed3075aca10336fa037c10251f
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-04 15:44:28 +0700

    Refactor ResetPasswordForm to use Bootstrap components and improve OTP input layout

--- EVCare_FrontEnd/src/pages/Auth/sections/ResetPasswordForm.tsx
+++ EVCare_FrontEnd/src/pages/Auth/sections/ResetPasswordForm.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { ONE_NUMBER_REGEX } from "../../../constants/regexs/NumberRegex";
-import { FormWrapperOTP, SubmitBtn } from "../Authentication.styled";
+import { FieldGroup, FormWrapper, SubmitBtn } from "../Authentication.styled";
 import SpinnerComponent from "../../../components/SpinnerComponent";
+import { Form } from "react-bootstrap";
 
 type FormDataResetPassword = {
   newPassword: string;
@@ -48,82 +49,71 @@ export default function ResetPasswordForm({ handleSubmit, disable }: ResetPasswo
     handleSubmit(formData, finalOtp);
   };
 
-  const inputStyle: React.CSSProperties = {
-    width: "100%",
-    height: "50px",
-    fontSize: "1.2rem",
-    border: "1px solid #ccc",
-    padding: "0 10px",
-    marginBottom: "15px",
-    borderRadius: "4px",
-    boxSizing: "border-box",
-  };
-
   return (
-    <FormWrapperOTP>
-      <label htmlFor="newPassword" style={{ width: "100%", textAlign: "left", fontWeight: 500 }}>
-        New Password
-      </label>
-      <input
-        id="newPassword"
-        name="newPassword"
-        type="password"
-        style={inputStyle}
-        value={formData.newPassword}
-        onChange={handleInputChange}
-        disabled={disable}
-        placeholder="Input new password"
-      />
-
-      <label htmlFor="confirmNewPassword" style={{ width: "100%", textAlign: "left", fontWeight: 500 }}>
-        Confirm New Password
-      </label>
-      <input
-        id="confirmNewPassword"
-        name="confirmNewPassword"
-        type="password"
-        style={inputStyle}
-        value={formData.confirmNewPassword}
-        onChange={handleInputChange}
-        disabled={disable}
-        placeholder="Input confirm new password"
-      />
-
-      <label style={{ width: "100%", textAlign: "left", fontWeight: 500, marginTop: "10px" }}>
-        OTP Code (6 digits)
-      </label>
-      <p style={{ alignSelf: "flex-start", margin: "5px 0 10px 0" }}>Enter the 6-digit OTP sent to your email:</p>
-      <div style={{ display: "flex", gap: "10px", justifyContent: "center", marginBottom: "20px" }}>
-        {Array(6)
-          .fill(0)
-          .map((_, i) => (
-            <input
-              key={i}
-              type="text"
-              inputMode="numeric"
-              maxLength={1}
-              style={{
-                width: "45px",
-                height: "45px",
-                textAlign: "center",
-                fontSize: "1.5rem",
-                border: "1px solid #ccc",
-                borderRadius: "4px",
-              }}
-              value={otp[i]}
-              onChange={(e) => handleOtpChange(i, e.target.value)}
-              disabled={disable}
-            />
-          ))}
-      </div>
-
+    <FormWrapper>
+      <FieldGroup>
+        <Form.Label htmlFor="newPassword" style={{ fontWeight: 600 }}>
+          New Password
+        </Form.Label>
+        <Form.Control
+          id="newPassword"
+          name="newPassword"
+          type="password"
+          value={formData.newPassword}
+          onChange={handleInputChange}
+          disabled={disable}
+          placeholder="Input new password"
+        />
+      </FieldGroup>
+      <FieldGroup>
+        <Form.Label htmlFor="confirmNewPassword" style={{ fontWeight: 600 }}>
+          Confirm New Password
+        </Form.Label>
+        <Form.Control
+          id="confirmNewPassword"
+          name="confirmNewPassword"
+          type="password"
+          value={formData.confirmNewPassword}
+          onChange={handleInputChange}
+          disabled={disable}
+          placeholder="Input new password"
+        />
+      </FieldGroup>
+      <FieldGroup>
+        <Form.Label style={{ fontWeight: 600 }}>OTP Code (6 digits)</Form.Label>
+        <p style={{ margin: "5px 0 10px 0", color: "#6b7280", fontSize: "0.9rem" }}>
+          Enter the 6-digit OTP sent to your email:
+        </p>
+        <div style={{ display: "flex", gap: "10px", justifyContent: "center", marginBottom: "20px" }}>
+          {Array(6)
+            .fill(0)
+            .map((_, i) => (
+              <Form.Control
+                key={i}
+                type="text"
+                inputMode="numeric"
+                maxLength={1}
+                style={{
+                  width: "45px",
+                  height: "45px",
+                  textAlign: "center",
+                  fontSize: "1.5rem",
+                  padding: 0,
+                }}
+                value={otp[i]}
+                onChange={(e) => handleOtpChange(i, e.target.value)}
+                disabled={disable}
+              />
+            ))}
+        </div>
+      </FieldGroup>
       {disable ? (
         <SpinnerComponent />
       ) : (
         <SubmitBtn type="button" onClick={handleFormSubmit}>
-          Confirm
+          Confirm New Password
         </SubmitBtn>
       )}
-    </FormWrapperOTP>
+    </FormWrapper>
   );
 }
