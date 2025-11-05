f4530c3b1dea5c108f240d996e86b20fa12d596b
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-05 17:15:29 +0700

    feat: update Vitest configuration and add coverage reporting

--- EVCare_FrontEnd/vite.config.ts
+++ EVCare_FrontEnd/vite.config.ts
@@ -2,26 +2,54 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-/// <reference types="vitest" />
+export default defineConfig(({ mode }) => {
+  const isTestMode = mode === "test";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
+  return {
+    base: "/",
+    plugins: [
+      react({
+        babel: {
+          plugins: isTestMode ? [] : ["babel-plugin-react-compiler"],
+        },
+      }),
+      tailwindcss(),
+    ],
+
+    test: {
+      globals: false,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
+
+      coverage: {
+        provider: "istanbul",
+        enabled: true,
+        reporter: ["text", "html"],
+
+        exclude: [
+          "coverage/**",
+          "dist/**",
+          "**/*.config.js",
+          "**/*.config.ts",
+          "src/setupTests.ts",
+          "**/*.test.tsx",
+          "**/*.test.ts",
+          "**/*.styled.tsx",
+          "**/*.styled.ts",
+          "src/models/**",
+          "src/main.tsx",
+          "src/vite-env.d.ts",
+          "src/constants/**",
+          "src/context/**",
+          "src/token/**",
+          "src/api/**",
+        ],
       },
-    }),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-  },
+    },
+
+    server: {
+      port: 5173,
+      strictPort: true,
+    },
+  };
 });
