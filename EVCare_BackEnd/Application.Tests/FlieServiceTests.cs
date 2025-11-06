eef9aeb1fd08fd980d27fbe2047899ade496f5a5
slazenger1906 <npsdn1906@gmail.com>
2025-11-06 10:15:25 +0700

    done test for upload model 3d async

--- EVCare_BackEnd/Application.Tests/FlieServiceTests.cs
+++ EVCare_BackEnd/Application.Tests/FlieServiceTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.S3.Model;
 using Application.Service;
 using AutoFixture;
 using AutoFixture.AutoMoq;
@@ -14,6 +16,7 @@ using CloudinaryDotNet.Actions;
 using DataAccess.Dtos.File;
 using Microsoft.Extensions.Configuration;
 using Moq;
+using Moq.Protected;
 
 namespace Application.Tests {
     public class FlieServiceTests {
@@ -171,5 +174,158 @@ namespace Application.Tests {
             Assert.Equal("https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg", result[1].Url);
 
         }
-     }
+
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileIsNull_ThrowsException() {
+            var fileService = CreateFileService();
+            var result = await
+                Assert.ThrowsAsync<ArgumentNullException>(async () => await fileService.UploadModel3DAsync(null));
+            Assert.NotNull(result);
+
+        }
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileNameIsNull_ThrowsException() {
+            var fileService = CreateFileService();
+            FileUploadModel file = new FileUploadModel
+            {
+                ContentType = "",
+                FileStream = new MemoryStream(Encoding.UTF8.GetBytes("dummy image content")),
+                FolderName = "abc"
+            };
+            var result = await
+               Assert.ThrowsAsync<Exception>(async () => await fileService.UploadModel3DAsync(file));
+            Assert.NotNull(result);
+            Assert.Equal("Missing FileName.", result.Message);
+        }
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileNameIsEmpty_ThrowsException() {
+            var fileService = CreateFileService();
+            FileUploadModel file = new FileUploadModel
+            {
+                ContentType = "",
+                FileStream = new MemoryStream(Encoding.UTF8.GetBytes("dummy image content")),
+                FolderName = "abc",
+                FileName = ""
+            };
+            var result = await
+               Assert.ThrowsAsync<Exception>(async () => await fileService.UploadModel3DAsync(file));
+            Assert.NotNull(result);
+            Assert.Equal("Missing FileName.", result.Message);
+        }
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileStreamIsNull_ThrowsException() {
+            var fileService = CreateFileService();
+            FileUploadModel file = new FileUploadModel
+            {
+                ContentType = "",
+                FileStream = null,
+                FolderName = "abc",
+                FileName = "abc"
+            };
+            var result = await
+               Assert.ThrowsAsync<Exception>(async () => await fileService.UploadModel3DAsync(file));
+            Assert.NotNull(result);
+            Assert.Equal("Invalid FileStream.", result.Message);
+        }
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileNameDontHaveExtension_ThrowsException() {
+            var fileService = CreateFileService();
+            FileUploadModel file = new FileUploadModel
+            {
+                ContentType = "",
+                FileStream = new MemoryStream(Encoding.UTF8.GetBytes("dummy image content")),
+                FolderName = "abc",
+                FileName = "abc"
+            };
+            var result = await
+               Assert.ThrowsAsync<Exception>(async () => await fileService.UploadModel3DAsync(file));
+            Assert.NotNull(result);
+            Assert.Equal("File type is not allowed", result.Message);
+        }
+
+        [Fact]
+        public async Task UploadModel3DAsync_WithFileNameHaveInvalidExtension_ThrowsException() {
+            var fileService = CreateFileService();
+            FileUploadModel file = new FileUploadModel
+            {
+                ContentType = "",
+                FileStream = new MemoryStream(Encoding.UTF8.GetBytes("dummy image content")),
+                FolderName = "abc",
+                FileName = "abc.txt"
+            };
+            var result = await
+               Assert.ThrowsAsync<Exception>(async () => await fileService.UploadModel3DAsync(file));
+            Assert.NotNull(result);
+            Assert.Equal("File type is not allowed", result.Message);
+        }
+
+        [Fact]
+        public async Task UploadModel3DAsync_WithVaild_AddSuccessfully() {
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x["Cloudinary:CloudName"]).Returns("abc");
+            configMock.SetupGet(x => x["Cloudinary:ApiKey"]).Returns("123");
+            configMock.SetupGet(x => x["Cloudinary:ApiSecret"]).Returns("456");
+            configMock.SetupGet(x => x["R2:AccountId"]).Returns("acc");
+            configMock.SetupGet(x => x["R2:AccessKeyId"]).Returns("ak");
+            configMock.SetupGet(x => x["R2:SecretAccessKey"]).Returns("sk");
+            configMock.SetupGet(x => x["R2:Bucket"]).Returns("bucket-3d");
+            configMock.SetupGet(x => x["R2:PublicBaseUrl"]).Returns("https://cdn.com");
+            var serviceMock = new Mock<FileService>(configMock.Object) { CallBase = true };
+
+            serviceMock
+          .Protected()
+          .Setup<Task>("PutToS3Async", ItExpr.IsAny<PutObjectRequest>())
+          .Returns(Task.CompletedTask);
+
+            var stream = new MemoryStream(new byte[] { 1, 2, 3 });
+            var uploadModel = new FileUploadModel
+            {
+                FileName = "car.glb",
+                FileStream = stream,
+                FolderName = "models",
+                ContentType = "model/gltf-binary"
+            };
+            var resultUrl = await serviceMock.Object.UploadModel3DAsync(uploadModel);
+            Assert.StartsWith("https://cdn.com/models/", resultUrl);
+            Assert.EndsWith(".glb", resultUrl);
+        }
+        public class NonSeekableStream : MemoryStream {
+            public override bool CanSeek => false;
+            public NonSeekableStream(byte[] buffer) : base(buffer) { }
+        }
+        [Fact]
+        public async Task UploadModel3DAsync_WhenStreamIsNonSeekable_CopiesToMemoryStream() {
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x["Cloudinary:CloudName"]).Returns("abc");
+            configMock.SetupGet(x => x["Cloudinary:ApiKey"]).Returns("123");
+            configMock.SetupGet(x => x["Cloudinary:ApiSecret"]).Returns("456");
+            configMock.SetupGet(x => x["R2:AccountId"]).Returns("acc");
+            configMock.SetupGet(x => x["R2:AccessKeyId"]).Returns("ak");
+            configMock.SetupGet(x => x["R2:SecretAccessKey"]).Returns("sk");
+            configMock.SetupGet(x => x["R2:Bucket"]).Returns("bucket-3d");
+            configMock.SetupGet(x => x["R2:PublicBaseUrl"]).Returns("https://cdn.com");
+            var serviceMock = new Mock<FileService>(configMock.Object) { CallBase = true };
+
+            serviceMock
+          .Protected()
+          .Setup<Task>("PutToS3Async", ItExpr.IsAny<PutObjectRequest>())
+          .Returns(Task.CompletedTask);
+            var buffer = new byte[] { 10, 20, 30, 40 };
+            var nonSeekStream = new NonSeekableStream(buffer);
+            var uploadModel = new FileUploadModel
+            {
+                FileName = "car.glb",
+                FileStream = nonSeekStream,
+                FolderName = "models",
+                ContentType = "model/gltf-binary"
+            };
+            var resultUrl = await serviceMock.Object.UploadModel3DAsync(uploadModel);
+            Assert.StartsWith("https://cdn.com/models/", resultUrl);
+            Assert.EndsWith(".glb", resultUrl);
+
+        }
+
+
+
+    }
 }
