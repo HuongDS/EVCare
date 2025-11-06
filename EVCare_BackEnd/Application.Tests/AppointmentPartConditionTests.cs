b73783030d71478fb2588593816ae1f7d7a083fe
slazenger1906 <npsdn1906@gmail.com>
2025-11-06 10:59:28 +0700

    done test for update appointment part condition async

--- EVCare_BackEnd/Application.Tests/AppointmentPartConditionTests.cs
+++ EVCare_BackEnd/Application.Tests/AppointmentPartConditionTests.cs
@@ -57,6 +57,49 @@ namespace Application.Tests {
 
         }
 
+        [Theory,AutoData]
+        public async Task UpdateAppointmentPartConditionAsync_WithValidData_UpdateSuccessfully(
+            AppointmentPartConditionCreateModel dto, int technicianId) {
+            var uowMock = _fixture.Freeze<Mock<IUnitOfWork>>();
+            uowMock
+                .Setup(u => u.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()))
+                .Returns<Func<Task>>(action => action());
+            var appointmentPartConditionRepository = _fixture.Freeze<Mock<IAppointmentPartConditionRepository>>();
+            appointmentPartConditionRepository.Setup(t =>
+            t.CreateAppointmentPartConditionAsync(It.IsAny<AppointmentPartCondition>())).Returns(Task.CompletedTask);
+
+            appointmentPartConditionRepository.Setup(t=>
+            t.DeleteAppointmentPartConditionsByAppointmentIdAsync(It.IsAny<int>(),It.IsAny<int>())
+            ).Returns(Task.CompletedTask);
+
+            dto.PartDamageLevels = new List<AppointmentPartDamageLevelCreateModel>
+            {
+                new AppointmentPartDamageLevelCreateModel()
+                {
+                    PartId = 1,
+                    LevelEnum = DataAccess.Enums.DamageLevelEnum.Moderate
+                },
+                 new AppointmentPartDamageLevelCreateModel()
+                {
+                    PartId = 2,
+                    LevelEnum = DataAccess.Enums.DamageLevelEnum.Severe
+                },
+                  new AppointmentPartDamageLevelCreateModel()
+                {
+                    PartId = 3,
+                    LevelEnum = DataAccess.Enums.DamageLevelEnum.Severe
+                }
+            };
+            var appointmentPartContitionService = _fixture.Create<AppointmentPartConditionService>();
+
+            await appointmentPartContitionService.UpdateAppointmentPartConditionAsync(dto, technicianId);
+            appointmentPartConditionRepository.Verify(t =>
+            t.CreateAppointmentPartConditionAsync(It.IsAny<AppointmentPartCondition>()), Times.Exactly(3));
+
+
+
+        }
+
 
 
     }
