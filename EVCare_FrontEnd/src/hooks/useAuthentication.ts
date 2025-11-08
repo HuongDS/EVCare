a2e0fc2621bb7fe44e59f2326a9e95b3f9b8cfc2
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-08 13:07:56 +0700

    feat: implement authentication logic using useAuthentication hook and add NAME_REGEX for name validation

---
+++ EVCare_FrontEnd/src/hooks/useAuthentication.ts
@@ -0,0 +1,325 @@
+import { useCallback, useMemo, useState } from "react";
+import { AUTH_FORM_MESSAGE, ERROR_MESSAGE, FORM_MESSAGES, MSG_TITLE } from "../constants/messages/Message";
+import { handleError } from "../utils/errorHandler";
+import HTTP_STATUS from "../constants/Code/HttpStatusCode";
+import { EMAIL_REGEX } from "../constants/regexs/EmailRegex";
+import { closeLogin, consumeAction, openAppointmentForm } from "../states/uiSlice";
+import { login, register, resetPassword, saveTokens, sendOtp } from "../services/authService";
+import type { LoginRequestDto, RegisterRequestDto, VerifyOTPDto } from "../models/AuthModel/authModel";
+import { PASSWORD_REGEX } from "../constants/regexs/PasswordRegex";
+import { LENGTH } from "../constants/Code/Constants";
+import { OTP_REGEX } from "../constants/regexs/OTPRegex";
+import { PHONE_NUMBER_REGEX } from "../constants/regexs/PhoneNumberRegex";
+import { NAME_REGEX } from "../constants/regexs/NameRegex";
+import { ACTION } from "../constants/messages/Actions";
+import { loginSuccess } from "../states/authSlice";
+import { RoleEnum } from "../models/enums";
+import { saveUser } from "../token/tokenStore";
+import { toUseFromJwt } from "../token/jwtDecode";
+import { useNotification } from "../context/useNotification";
+import { useNavigate } from "react-router";
+import type { AppDispatch, RootState } from "../states/store";
+import { useDispatch, useSelector } from "react-redux";
+
+type FormDataResetPassword = {
+  newPassword: string;
+  confirmNewPassword: string;
+};
+
+export const useAuthentication = () => {
+  const [isSignUp, setIsSignUp] = useState(true); // true: signUp | false : login
+  const [isOTP, setIsOTP] = useState(false); // true: verify Otp
+
+  // Form states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isForgot, setIsForgot] = useState(false);
+
+  // Redux
+  const dispatch = useDispatch<AppDispatch>();
+  const pending = useSelector((state: RootState) => state.ui.actionAfterLogin);
+  const loginFormOpen = useSelector((state: RootState) => state.ui.loginFormOpen);
+
+  // Navigate
+  const navigate = useNavigate();
+
+  // notification
+  const notification = useNotification();
+
+  // login
+  const handleLogin = useCallback(async () => {
+    setIsLoading(true);
+    const loginData: LoginRequestDto = {
+      email: email.trim(),
+      password: password.trim(),
+    };
+    try {
+      if (!EMAIL_REGEX.test(email)) {
+        throw new Error(ERROR_MESSAGE.INVALID_EMAIL);
+      } else if (!PASSWORD_REGEX.test(password)) {
+        throw new Error(ERROR_MESSAGE.INVALID_PASSWORD);
+      }
+      const response = await login(loginData);
+      if (response.statusCode !== HTTP_STATUS.OK) {
+        throw new Error(ERROR_MESSAGE.LOGIN_FAILED);
+      }
+      const token = response.data?.accessToken;
+      if (!token) {
+        throw new Error(ERROR_MESSAGE.SOME_THING_WENT_WRONG);
+      }
+      saveTokens(token);
+      const user = toUseFromJwt(token);
+      saveUser(user);
+
+      notification.success({
+        message: MSG_TITLE.LOGIN,
+        description: response.message,
+        showProgress: true,
+      });
+
+      // Author
+      switch (user.role) {
+        case RoleEnum.ADMIN:
+          navigate("/admin/general");
+          break;
+        case RoleEnum.STAFF:
+          navigate("/staff");
+          break;
+        case RoleEnum.TECHNICIAN:
+          navigate("/technician");
+          break;
+      }
+
+      dispatch(loginSuccess(user));
+      dispatch(closeLogin());
+      setEmail("");
+      setPassword("");
+      if (pending === ACTION.OPEN_APPOINTMENT) {
+        dispatch(openAppointmentForm());
+      }
+      dispatch(consumeAction());
+      setIsLoading(false);
+      // alert(response.message);
+    } catch (err) {
+      notification.error({
+        message: MSG_TITLE.LOGIN,
+        description: (err as Error).message,
+        showProgress: true,
+      });
+      setIsLoading(false);
+      return;
+    }
+  }, [email, password, pending, dispatch, navigate, notification]);
+
+  const handleSignUp = useCallback(async () => {
+    if (firstName == null || lastName == null || firstName.length === 0 || lastName.length === 0) {
+      notification.warning({
+        message: "Error",
+        description: FORM_MESSAGES.NAME,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    } else if (
+      firstName.trim().length == 0 ||
+      lastName.trim().length == 0 ||
+      !NAME_REGEX.test(firstName) ||
+      !NAME_REGEX.test(lastName)
+    ) {
+      notification.warning({
+        message: "Error",
+        description: FORM_MESSAGES.INVALID_NAME,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    } else if (!EMAIL_REGEX.test(email)) {
+      notification.warning({
+        message: "Error",
+        description: ERROR_MESSAGE.INVALID_EMAIL,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    } else if (!PASSWORD_REGEX.test(password) || !PASSWORD_REGEX.test(confirm)) {
+      notification.warning({
+        message: "Error",
+        description: ERROR_MESSAGE.INVALID_PASSWORD,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    } else if (!PHONE_NUMBER_REGEX.test(phone)) {
+      notification.warning({
+        message: "Error",
+        description: ERROR_MESSAGE.INVALID_PHONE,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    } else if (password !== confirm) {
+      notification.warning({
+        message: "Error",
+        description: ERROR_MESSAGE.PASSWORD_AND_CONFIRM_PASSWORD_MUST_BE_SAME,
+        showProgress: true,
+        duration: 3,
+      });
+      return;
+    }
+    const registerData: RegisterRequestDto = {
+      email: email.trim(),
+      password: password.trim(),
+      firstName: firstName.trim(),
+      lastName: lastName.trim(),
+      phone: phone.trim(),
+    };
+    setIsLoading(true);
+    try {
+      const response = await register(registerData);
+      if (!response) {
+        throw new Error(response);
+      }
+      setFirstName("");
+      setLastName("");
+      setPassword("");
+      setConfirm("");
+      setPhone("");
+      setIsOTP(true);
+      setIsLoading(false);
+    } catch (error) {
+      notification.error({
+        message: (error as Error).message,
+        showProgress: true,
+      });
+      handleError(error);
+      setIsLoading(false);
+      return;
+    }
+  }, [email, password, firstName, lastName, phone, confirm]);
+
+  const handleSubmitResetPassword = useCallback(
+    async (formData: FormDataResetPassword, otp: string) => {
+      setIsLoading(true);
+      if (otp.length != LENGTH.OTP_LENGTH || !OTP_REGEX.test(otp)) {
+        notification.warning({
+          message: `OTP must be ${LENGTH.OTP_LENGTH} numbers`,
+          showProgress: true,
+        });
+        return;
+      }
+      if (!PASSWORD_REGEX.test(formData.newPassword)) {
+        notification.warning({
+          message: ERROR_MESSAGE.INVALID_PASSWORD,
+          showProgress: true,
+        });
+        return;
+      }
+      if (formData.newPassword !== formData.confirmNewPassword) {
+        notification.warning({
+          message: ERROR_MESSAGE.PASSWORD_AND_CONFIRM_PASSWORD_MUST_BE_SAME,
+          showProgress: true,
+        });
+        return;
+      }
+      try {
+        const data: VerifyOTPDto = {
+          email: email,
+          otp: otp,
+          newPassword: formData.newPassword,
+        };
+        const response = await resetPassword(data);
+        notification.success({ message: response.message });
+      } catch (error) {
+        notification.error({
+          message: (error as Error).message,
+          showProgress: true,
+        });
+        setIsLoading(false);
+        handleError(error);
+        return;
+      }
+      setIsLoading(false);
+      setIsOTP(false);
+      dispatch(closeLogin());
+    },
+    [email, dispatch]
+  );
+
+  const handChangeIsForgot = useCallback(async () => {
+    setIsLoading(true);
+    setIsForgot(true);
+    try {
+      if (!EMAIL_REGEX.test(email.trim())) {
+        notification.warning({ message: ERROR_MESSAGE.INVALID_EMAIL });
+        setIsLoading(false);
+        setIsForgot(false);
+        return;
+      }
+      const response = await sendOtp(email);
+      if (response.statusCode === HTTP_STATUS.OK) {
+        notification.success({ message: response.message });
+        setIsOTP(true);
+        setIsForgot(false);
+      }
+    } catch (error) {
+      notification.error({
+        message: (error as Error).message,
+        showProgress: true,
+      });
+      handleError(error);
+      setIsForgot(false);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  }, [email, notification]);
+
+  // header text
+  const headerText = useMemo(() => {
+    if (isOTP) return AUTH_FORM_MESSAGE.VERIFY;
+    if (isSignUp) return AUTH_FORM_MESSAGE.REGISTER;
+    if (isForgot) return AUTH_FORM_MESSAGE.FORGOT_PASSWORD;
+    return AUTH_FORM_MESSAGE.LOGIN;
+  }, [isOTP, isSignUp, isForgot]);
+
+  return {
+    isSignUp,
+    isOTP,
+    email,
+    password,
+    confirm,
+    firstName,
+    lastName,
+    phone,
+    isLoading,
+    isForgot,
+    pending,
+
+    navigate,
+    notification,
+    dispatch,
+    loginFormOpen,
+
+    setIsSignUp,
+    setIsOTP,
+    setEmail,
+    setPassword,
+    setConfirm,
+    setFirstName,
+    setLastName,
+    setPhone,
+    setIsLoading,
+    setIsForgot,
+
+    handleLogin,
+    handleSignUp,
+    handleSubmitResetPassword,
+    handChangeIsForgot,
+    headerText,
+  };
+};
--- EVCare_FrontEnd/src/pages/Auth/Authentication.tsx
+++ EVCare_FrontEnd/src/pages/Auth/Authentication.tsx
@@ -1,296 +1,48 @@
-import { useCallback, useMemo, useState } from "react";
 import logo from "../../assets/EVCare.png";
 import SwitchButton from "../../components/Button/SwitchButton";
 import { FormContainer, HeaderBox, SideImage, StyledModal } from "./Authentication.styled";
 import AuthForm from "./sections/AuthForm";
 import ResetPasswordForm from "./sections/ResetPasswordForm";
-import { AUTH_FORM_MESSAGE, ERROR_MESSAGE, FORM_MESSAGES, MSG_TITLE } from "../../constants/messages/Message";
-import type { LoginRequestDto, RegisterRequestDto, VerifyOTPDto } from "../../models/AuthModel/authModel";
-import { login, register, resetPassword, saveTokens, sendOtp } from "../../services/authService";
-import { toUseFromJwt } from "../../token/jwtDecode";
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "../../states/store";
-import { loginSuccess } from "../../states/authSlice";
-import { LENGTH } from "../../constants/Code/Constants";
-import { OTP_REGEX } from "../../constants/regexs/OTPRegex";
-import { saveUser } from "../../token/tokenStore";
-import { PASSWORD_REGEX } from "../../constants/regexs/PasswordRegex";
-import { EMAIL_REGEX } from "../../constants/regexs/EmailRegex";
-import { PHONE_NUMBER_REGEX } from "../../constants/regexs/PhoneNumberRegex";
-import { closeLogin, consumeAction, openAppointmentForm } from "../../states/uiSlice";
-import { ACTION } from "../../constants/messages/Actions";
-import HTTP_STATUS from "../../constants/Code/HttpStatusCode";
-import { handleError } from "../../utils/errorHandler";
+import { closeLogin } from "../../states/uiSlice";
 import ForgotPassword from "./sections/ForgotPassword";
-import { RoleEnum } from "../../models/enums";
-import { useNavigate } from "react-router";
-import { useNotification } from "../../context/useNotification";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 // interface AuthProps {
 //   show: boolean;
 //   handleClose: () => void;
 // }
 
-type FormDataResetPassword = {
-  newPassword: string;
-  confirmNewPassword: string;
-};
-
 export default function Authentication() {
-  const [isSignUp, setIsSignUp] = useState(true); // true: signUp | false : login
-  const [isOTP, setIsOTP] = useState(false); // true: verify Otp
+  const {
+    isSignUp,
+    isOTP,
+    email,
+    password,
+    confirm,
+    firstName,
+    lastName,
+    phone,
+    isLoading,
+    isForgot,
 
-  // Form states
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm, setConfirm] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [isForgot, setIsForgot] = useState(false);
+    dispatch,
+    loginFormOpen,
 
-  // Redux
-  const dispatch = useDispatch<AppDispatch>();
-  const pending = useSelector((state: RootState) => state.ui.actionAfterLogin);
-  const loginFormOpen = useSelector((state: RootState) => state.ui.loginFormOpen);
+    setIsSignUp,
+    setEmail,
+    setPassword,
+    setConfirm,
+    setFirstName,
+    setLastName,
+    setPhone,
+    setIsForgot,
 
-  // Navigate
-  const navigate = useNavigate();
-
-  // notification
-  const notification = useNotification();
-
-  // login
-  const handleLogin = useCallback(async () => {
-    setIsLoading(true);
-    const loginData: LoginRequestDto = {
-      email: email.trim(),
-      password: password.trim(),
-    };
-    try {
-      if (!EMAIL_REGEX.test(email)) {
-        throw new Error(ERROR_MESSAGE.INVALID_EMAIL);
-      } else if (!PASSWORD_REGEX.test(password)) {
-        throw new Error(ERROR_MESSAGE.INVALID_PASSWORD);
-      }
-      const response = await login(loginData);
-      if (response.statusCode !== HTTP_STATUS.OK) {
-        throw new Error(ERROR_MESSAGE.LOGIN_FAILED);
-      }
-      const token = response.data?.accessToken;
-      if (!token) {
-        throw new Error(ERROR_MESSAGE.SOME_THING_WENT_WRONG);
-      }
-      saveTokens(token);
-      const user = toUseFromJwt(token);
-      saveUser(user);
-
-      notification.success({
-        message: MSG_TITLE.LOGIN,
-        description: response.message,
-        showProgress: true,
-      });
-
-      // Author
-      switch (user.role) {
-        case RoleEnum.ADMIN:
-          navigate("/admin/general");
-          break;
-        case RoleEnum.STAFF:
-          navigate("/staff");
-          break;
-        case RoleEnum.TECHNICIAN:
-          navigate("/technician");
-          break;
-      }
-
-      dispatch(loginSuccess(user));
-      dispatch(closeLogin());
-      setEmail("");
-      setPassword("");
-      if (pending === ACTION.OPEN_APPOINTMENT) {
-        dispatch(openAppointmentForm());
-      }
-      dispatch(consumeAction());
-      setIsLoading(false);
-      // alert(response.message);
-    } catch (err) {
-      notification.error({
-        message: MSG_TITLE.LOGIN,
-        description: (err as Error).message,
-        showProgress: true,
-      });
-      setIsLoading(false);
-      return;
-    }
-  }, [email, password, pending, dispatch, navigate, notification]);
-
-  const handleSignUp = useCallback(async () => {
-    if (firstName == null || lastName == null || firstName.length === 0 || lastName.length === 0) {
-      notification.warning({
-        message: "Error",
-        description: FORM_MESSAGES.NAME,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    } else if (firstName.trim().length == 0 || lastName.trim().length == 0) {
-      notification.warning({
-        message: "Error",
-        description: FORM_MESSAGES.NAME,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    } else if (!EMAIL_REGEX.test(email)) {
-      notification.warning({
-        message: "Error",
-        description: ERROR_MESSAGE.INVALID_EMAIL,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    } else if (!PASSWORD_REGEX.test(password) || !PASSWORD_REGEX.test(confirm)) {
-      notification.warning({
-        message: "Error",
-        description: ERROR_MESSAGE.INVALID_PASSWORD,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    } else if (!PHONE_NUMBER_REGEX.test(phone)) {
-      notification.warning({
-        message: "Error",
-        description: ERROR_MESSAGE.INVALID_PHONE,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    } else if (password !== confirm) {
-      notification.warning({
-        message: "Error",
-        description: ERROR_MESSAGE.PASSWORD_AND_CONFIRM_PASSWORD_MUST_BE_SAME,
-        showProgress: true,
-        duration: 3,
-      });
-      return;
-    }
-    const registerData: RegisterRequestDto = {
-      email: email.trim(),
-      password: password.trim(),
-      firstName: firstName.trim(),
-      lastName: lastName.trim(),
-      phone: phone.trim(),
-    };
-    setIsLoading(true);
-    try {
-      const response = await register(registerData);
-      if (!response) {
-        throw new Error(response);
-      }
-      setFirstName("");
-      setLastName("");
-      setPassword("");
-      setConfirm("");
-      setPhone("");
-      setIsOTP(true);
-      setIsLoading(false);
-    } catch (error) {
-      notification.error({
-        message: (error as Error).message,
-        showProgress: true,
-      });
-      handleError(error);
-      setIsLoading(false);
-      return;
-    }
-  }, [email, password, firstName, lastName, phone, confirm]);
-
-  const handleSubmitResetPassword = useCallback(
-    async (formData: FormDataResetPassword, otp: string) => {
-      setIsLoading(true);
-      if (otp.length != LENGTH.OTP_LENGTH || !OTP_REGEX.test(otp)) {
-        notification.warning({
-          message: `OTP must be ${LENGTH.OTP_LENGTH} numbers`,
-          showProgress: true,
-        });
-        return;
-      }
-      if (!PASSWORD_REGEX.test(formData.newPassword)) {
-        notification.warning({
-          message: ERROR_MESSAGE.INVALID_PASSWORD,
-          showProgress: true,
-        });
-        return;
-      }
-      if (formData.newPassword !== formData.confirmNewPassword) {
-        notification.warning({
-          message: ERROR_MESSAGE.PASSWORD_AND_CONFIRM_PASSWORD_MUST_BE_SAME,
-          showProgress: true,
-        });
-        return;
-      }
-      try {
-        const data: VerifyOTPDto = {
-          email: email,
-          otp: otp,
-          newPassword: formData.newPassword,
-        };
-        const response = await resetPassword(data);
-        notification.success({ message: response.message });
-      } catch (error) {
-        notification.error({
-          message: (error as Error).message,
-          showProgress: true,
-        });
-        setIsLoading(false);
-        handleError(error);
-        return;
-      }
-      setIsLoading(false);
-      setIsOTP(false);
-      dispatch(closeLogin());
-    },
-    [email, dispatch]
-  );
-
-  const handChangeIsForgot = useCallback(async () => {
-    setIsLoading(true);
-    setIsForgot(true);
-    try {
-      if (!EMAIL_REGEX.test(email.trim())) {
-        notification.warning({ message: ERROR_MESSAGE.INVALID_EMAIL });
-        setIsLoading(false);
-        setIsForgot(false);
-        return;
-      }
-      const response = await sendOtp(email);
-      if (response.statusCode === HTTP_STATUS.OK) {
-        notification.success({ message: response.message });
-        setIsOTP(true);
-        setIsForgot(false);
-      }
-    } catch (error) {
-      notification.error({
-        message: (error as Error).message,
-        showProgress: true,
-      });
-      handleError(error);
-      setIsForgot(false);
-      return;
-    } finally {
-      setIsLoading(false);
-    }
-  }, [email, notification]);
-
-  // header text
-  const headerText = useMemo(() => {
-    if (isOTP) return AUTH_FORM_MESSAGE.VERIFY;
-    if (isSignUp) return AUTH_FORM_MESSAGE.REGISTER;
-    if (isForgot) return AUTH_FORM_MESSAGE.FORGOT_PASSWORD;
-    return AUTH_FORM_MESSAGE.LOGIN;
-  }, [isOTP, isSignUp, isForgot]);
+    handleLogin,
+    handleSignUp,
+    handleSubmitResetPassword,
+    handChangeIsForgot,
+    headerText,
+  } = useAuthentication();
 
   return (
     <>
