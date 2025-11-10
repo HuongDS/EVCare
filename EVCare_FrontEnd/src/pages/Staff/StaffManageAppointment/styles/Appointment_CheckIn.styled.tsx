776afa47fa2db227e4577172d5269b23a98a97dd
Vy Ngo Chi <ncv.ngovy@gmail.com>
2025-11-10 07:26:46 +0700

    Update: re-style for Check In page

--- EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/styles/Appointment_CheckIn.styled.tsx
+++ EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/styles/Appointment_CheckIn.styled.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
-  max-height: 100vh;
+  max-height: fit-content;
   background: linear-gradient(135deg, #e8f5e9 0%, #f1f8e9 100%);
   padding: 32px 20px;
   font-family: "Outfit", sans-serif;
