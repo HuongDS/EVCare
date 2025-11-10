d6d41c26e8772d6b95c4115ba04b143d082d1c67
slazenger1906 <npsdn1906@gmail.com>
2025-11-10 22:17:40 +0700

    done test for get appoinemnt detail for technician

--- EVCare_BackEnd/IntegrationTests/AppointmentRepositoryTests.cs
+++ EVCare_BackEnd/IntegrationTests/AppointmentRepositoryTests.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 using DataAccess.Dtos.Pagination;
 using DataAccess.Enums;
 using DataAccess.Repositories;
+using Xunit.Abstractions;
 
 namespace IntegrationTests {
     public class AppointmentRepositoryTests : IClassFixture<DataBaseFixture> {
         private readonly DataBaseFixture _fixture;
         private readonly AppointmentRepository _repository;
+        private readonly ITestOutputHelper _output;
 
-        public AppointmentRepositoryTests(DataBaseFixture fixture) {
+        public AppointmentRepositoryTests(DataBaseFixture fixture, ITestOutputHelper output) {
             _fixture = fixture;
             _repository = new AppointmentRepository(_fixture.CreateContext());
         }
@@ -38,5 +40,37 @@ namespace IntegrationTests {
             Assert.Equal("Phuc Sanh", result.Items.First().CustomerName);
             Assert.Single(result.Items.First().Services);
         }
+        
+        [Fact]
+        public async Task GetAppointmentTechnicianViewModelByTechnicianId_WithValidData_ReturnsAppointments() {
+           
+            var query = new AppointmentTechnicianQueryDto
+            {
+                Status = TechnicianWorkingSessionEnum.InProgress,
+                PageIndex = 1,
+                PageSize = 5,
+                SortField = new string[] { "Appointment_Date" },
+                SortOrder = new string[] { "asc" }
+            };
+
+            int technicianId = 1;
+
+           
+            var result = await _repository.GetAppointmentTechnicianViewModelByTechnicianId(technicianId, query);
+
+           
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result.Items);
+            var appointment = result.Items.First();
+
+            Assert.Equal("ABC123", appointment.LicensePlate);
+            Assert.Equal("Phuc Sanh", appointment.CustomerName);
+            Assert.Equal("Sedan", appointment.VehicleModel);
+            Assert.Contains("Oil Change", appointment.Services);
+            Assert.Single(appointment.Parts);
+            Assert.Equal(TechnicianWorkingSessionEnum.InProgress, appointment.Status);
+        }
+
     }
 }
--- EVCare_BackEnd/IntegrationTests/DataBaseFixture.cs
+++ EVCare_BackEnd/IntegrationTests/DataBaseFixture.cs
@@ -30,46 +30,31 @@ namespace IntegrationTests {
 
         private void SeedTestData(EVCareDbContext context) {
 
-            var account = new Account
-            {
-                Id = 1,
-                First_Name = "Phuc",
-                Last_Name = "Sanh",
-                Phone = "0901234567",
-                Email = "npsdn1906@gmail.com"
+            var accountCustomer = new Account { Id = 1, First_Name = "Phuc", Last_Name = "Sanh", Phone = "0901234567", Email = "phuc@example.com" };
+            var accountEmployee = new Account { Id = 2, First_Name = "Huong", Last_Name = "Tran", Phone = "0902222333", Email = "huongtran@example.com" };
+
+            var customer = new Customer { Id = 1, AccountId = 1, Account = accountCustomer };
+            var employee = new Employee {
+                Id = 1, AccountId = 2, Account = accountEmployee,
+                Status = EmployeeStatusEnum.Available 
+                ,CCCD = "123456789",
             };
 
-       
-            var customer = new Customer
-            {
-                Id = 1,
-                AccountId = 1,
-                Account = account
-            };
+         
+            var technician = new Technician { Id = 1, EmployeeId = 1, Employee = employee, ExpYear = 5 };
+
+         
+            var category = new VehiclesCategory { Id = 1, Name = "Sedan" };
+            var vehicle = new Vehicle { Id = 1, LicensePlate = "ABC123", CategoryId = 1, Category = category };
+
+         
+            var oilChange = new Service { Id = 1, Name = "Oil Change", Description = "Thay dầu nhớt" };
+            var brakeCheck = new Service { Id = 2, Name = "Brake Check", Description = "Kiểm tra phanh" };
 
         
-            var category = new VehiclesCategory
-            {
-                Id = 1,
-                Name = "Sedan"
-            };
+            var techSkill = new TechnicianSkill {   TechnicianId = 1, Technician = technician, ServiceId = 1, Service = oilChange };
 
-         
-            var vehicle = new Vehicle
-            {
-                Id = 1,
-                LicensePlate = "ABC123",
-                CategoryId = 1,
-                Category = category
-            };
-
-         
-            var service = new Service
-            {
-                Id = 1,
-                Name = "Oil Change",
-                Description  = "abczyx"
-            };
+        
             var appointment = new Appointment
             {
                 Id = 1,
@@ -79,23 +64,55 @@ namespace IntegrationTests {
                 Vehicle = vehicle,
                 CustomerId = 1,
                 Customer = customer,
+                
                 AppointmentServices = new List<AppointmentService>
-        {
-            new AppointmentService
-            {
-                AppointmentId = 1,
-                ServiceId = 1,
-                Service = service
-            }
-        }
+                    {
+                        new AppointmentService { AppointmentId = 1, ServiceId = 1, Service = oilChange }
+                    }
             };
 
-            context.Accounts.Add(account);
+       
+            var part = new Part { 
+                Id = 1, 
+                Name = "Engine Oil",
+                Image = "oil.png", Stock = 10, Price = 500, ReplacementPrice = 1000,
+                Description = "ABC" 
+            };
+            var order = new Order
+            {
+                Id = 1,
+                AppointmentId = 1,
+                Appointment = appointment,
+                OrderParts = new List<OrderPart>
+                    {
+                        new OrderPart { TechnicianId = 1,OrderId = 1, PartId = 1, Part = part, Quantity = 1, Price = 500, ReplacementPrice = 1000 }
+                    }
+            };
+            appointment.Order = order;
+            appointment.OrderId = order.Id;
+
+          
+            var tws = new TechnicianWorkingSession
+            {
+                OrderId = 1,
+                TechnicianId = 1,
+                Technician = technician,
+                Status = TechnicianWorkingSessionEnum.InProgress
+            };
+
+            context.Accounts.AddRange(accountCustomer, accountEmployee);
             context.Customers.Add(customer);
+            context.Employees.Add(employee);
+            context.Technicians.Add(technician);
             context.VehiclesCategories.Add(category);
             context.Vehicles.Add(vehicle);
-            context.Services.Add(service);
+            context.Services.AddRange(oilChange, brakeCheck);
+            context.TechnicianSkills.Add(techSkill);
+            context.Parts.Add(part);
+            context.Orders.Add(order);
             context.Appointments.Add(appointment);
+            context.OrderParts.Add(order.OrderParts.First());
+            context.TechnicianWorkingSessions.Add(tws);
 
             context.SaveChanges();
 
