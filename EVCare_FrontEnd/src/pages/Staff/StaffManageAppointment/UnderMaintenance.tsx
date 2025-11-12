8e0fa02a99ed8e7ad29b98e5f18470cd3e1efbe5
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-12 18:02:37 +0700

    add file

--- EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/UnderMaintenance.tsx
+++ EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/UnderMaintenance.tsx
@@ -30,8 +30,7 @@ export default function UnderMaintenance() {
 
         <ExpectedBox>
           <Calendar size={16} />
-          The technician is repairing the customer’s vehicle, please wait for
-          the next step!
+          The technician is repairing the customer’s vehicle, please wait for the next step!
         </ExpectedBox>
       </ContentCard>
     </PageContainer>
