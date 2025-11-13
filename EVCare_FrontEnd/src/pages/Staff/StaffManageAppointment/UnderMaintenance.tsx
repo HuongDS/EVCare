98680b1fb4c3ea2508c40369a99e1ca633534d38
Vy Ngo Chi <ncv.ngovy@gmail.com>
2025-11-13 13:00:17 +0700

    Update: Add signalR for order updating

--- EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/UnderMaintenance.tsx
+++ EVCare_FrontEnd/src/pages/Staff/StaffManageAppointment/UnderMaintenance.tsx
@@ -9,8 +9,16 @@ import {
   ProgressText,
   ExpectedBox,
 } from "./styles/UnderMaintenance.styled";
+import { useStaffGetSignalDoneAppointment } from "../../../hooks/useStaffHub";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function UnderMaintenance() {
+  const queryClient = useQueryClient();
+  useStaffGetSignalDoneAppointment<string>(async (type, data) => {
+    if (type === "UpdateOrderStatus" && data === "Technician Done Task.") {
+      await queryClient.invalidateQueries({ queryKey: ["AppointmentDetail"] });
+    }
+  });
   return (
     <PageContainer>
       <ContentCard>
@@ -30,7 +38,8 @@ export default function UnderMaintenance() {
 
         <ExpectedBox>
           <Calendar size={16} />
-          The technician is repairing the customer’s vehicle, please wait for the next step!
+          The technician is repairing the customer’s vehicle, please wait for
+          the next step!
         </ExpectedBox>
       </ContentCard>
     </PageContainer>
