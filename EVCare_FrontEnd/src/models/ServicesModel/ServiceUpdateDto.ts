88eaf79ad25f009f42c8ada393dc086d69ea89da
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-14 01:42:04 +0700

    update service

--- EVCare_FrontEnd/src/models/ServicesModel/ServiceUpdateDto.ts
+++ EVCare_FrontEnd/src/models/ServicesModel/ServiceUpdateDto.ts
@@ -1,7 +1,8 @@
-export type ServiceCreateDto = {
+export type ServiceUpdateDto = {
   name: string;
   description: string;
   duration: number;
   serviceCategoryId: number;
   partsIds: number[];
+  id: number;
 };
