c1aa0a564ba37ad8443fd6b65bb2b0870c4f692d
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-15 01:20:22 +0700

    clear comment

--- EVCare_FrontEnd/src/services/updateOrderPartApi.ts
+++ EVCare_FrontEnd/src/services/updateOrderPartApi.ts
@@ -1,17 +1,12 @@
-// src/services/updateOrderPartApi.ts
 import { useMutation } from "@tanstack/react-query";
 import { api } from "../api/api";
 import type { ResponseDto } from "../models/OrderPartModel/Order_Parts_Model";
 import type { UpdateOrderPartDto } from "../models/OrderPartModel/Order_Parts_Model";
 import { handleError } from "../utils/errorHandler";
 
-// Hàm gọi API PUT
 export async function updateOrderParts(payload: UpdateOrderPartDto) {
   try {
-    const response = await api.put<ResponseDto<number>>(
-      "/api/Order/parts",
-      payload
-    );
+    const response = await api.put<ResponseDto<number>>("/api/Order/parts", payload);
     return response.data;
   } catch (error) {
     handleError(error);
@@ -24,10 +19,7 @@ export const useUpdateOrderParts = () => {
     mutationKey: ["UpdateOrderParts"],
     mutationFn: async (payload: UpdateOrderPartDto) => {
       try {
-        const response = await api.put<ResponseDto<number>>(
-          "/api/Order/parts",
-          payload
-        );
+        const response = await api.put<ResponseDto<number>>("/api/Order/parts", payload);
         return response.data;
       } catch (error) {
         handleError(error);
