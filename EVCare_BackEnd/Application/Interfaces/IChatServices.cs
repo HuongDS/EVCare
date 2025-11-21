0d66d7fd8e0a550894e0cd1530b7b297fd4dbbf9
dangsonghuong010405@gmail.com <dangsonghuong010405@gmail.com>
2025-11-21 16:18:04 +0700

    fix error

--- EVCare_BackEnd/Application/Interfaces/IChatServices.cs
+++ EVCare_BackEnd/Application/Interfaces/IChatServices.cs
@@ -11,7 +11,7 @@ namespace Application.Interfaces
     {
         Task<Conversation?> GetConversationAsync(string convId);
         Task<List<Message>> GetHistoryAsync(string conversationId, int skip, int take);
-        Task MarkAsReadUpToAsync(string conversationId, string readerId, string upToMessageId);
+        //Task MarkAsReadUpToAsync(string conversationId, string readerId, string upToMessageId);
         Task<Message> SaveMessageAsync(string conversationId, string senderId, string text, List<Attachment> atts);
     }
 }
