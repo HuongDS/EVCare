f75a82c4a8c0cdd945d42f409b27fb47ed2af431
slazenger1906 <npsdn1906@gmail.com>
2025-11-22 15:02:55 +0700

    add final

--- EVCare_BackEnd/DataAccess/Dtos/Appointment/AppointmentTechnicianViewModel.cs
+++ EVCare_BackEnd/DataAccess/Dtos/Appointment/AppointmentTechnicianViewModel.cs
@@ -12,6 +12,7 @@ namespace DataAccess.Dtos.Appointment
     {
         public int Id { get; set; }
         public DateTime AppointmentDate { get; set; }
+        public int TechnicianWorkingSessionId { get; set; }
         public string? VehicleModel { get; set; }
         public string CustomerName { get; set; }
         public string? PhoneNumber { get; set; }
